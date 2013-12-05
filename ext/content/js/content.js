899931c3470f67d59286e02a7e4433da6016ef9d
Alexandru Plugaru <alexandru@gymglish.com>
2013-12-05 15:52:19 +0100

    4 space indentation

--- ext/content/js/content.js
+++ ext/content/js/content.js
@@ -11616,162 +11616,161 @@ App.autocomplete.quicktexts = [];
 App.autocomplete.cursorPosition = null;
 
 PubSub.subscribe('focus', function(action, element, gmailView) {
-  if (action === 'off') {
-    App.autocomplete.close();
-  }
+    if (action === 'off') {
+        App.autocomplete.close();
+    }
 });
 
-
 App.autocomplete.onKeyDown = function (e) {
-  // Press tab while in compose and tab pressed
-  if (App.data.inCompose && e.keyCode == KEY_TAB) {
-    if (App.autocomplete.isActive) {
-      // Simulate closing
-      App.autocomplete.close();
-      // Do not prevent default
-    } else {
-      e.preventDefault();
-      e.stopPropagation();
+    // Press tab while in compose and tab pressed
+    if (App.data.inCompose && e.keyCode == KEY_TAB) {
+        if (App.autocomplete.isActive) {
+            // Simulate closing
+            App.autocomplete.close();
+            // Do not prevent default
+        } else {
+            e.preventDefault();
+            e.stopPropagation();
 
-      App.autocomplete.onKey(e.keyCode, e);
+            App.autocomplete.onKey(e.keyCode, e);
+        }
     }
-  }
 
-  // Press control keys when autocomplete is active
-  if (App.autocomplete.isActive && ~[KEY_ENTER, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
-    e.preventDefault();
-    e.stopPropagation();
+    // Press control keys when autocomplete is active
+    if (App.autocomplete.isActive && ~[KEY_ENTER, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
+        e.preventDefault();
+        e.stopPropagation();
 
-    App.autocomplete.onKey(e.keyCode);
-  }
+        App.autocomplete.onKey(e.keyCode);
+    }
 
-  // Only prevent propagation as we'll handle escape on keyup
-  // because well have to set autocomplete.active as false and it will propagate on keyup
-  if (App.autocomplete.isActive && e.keyCode == KEY_ESCAPE) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
+    // Only prevent propagation as we'll handle escape on keyup
+    // because well have to set autocomplete.active as false and it will propagate on keyup
+    if (App.autocomplete.isActive && e.keyCode == KEY_ESCAPE) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
 
-  // If dropdown is active but the pressed key is different from what we expect
-  if (App.autocomplete.isActive && !~[KEY_TAB, KEY_ENTER, KEY_ESCAPE, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
-    App.autocomplete.close();
-  }
+    // If dropdown is active but the pressed key is different from what we expect
+    if (App.autocomplete.isActive && !~[KEY_TAB, KEY_ENTER, KEY_ESCAPE, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
+        App.autocomplete.close();
+    }
 };
 
 App.autocomplete.onKeyUp = function(e) {
-  // Allways prevent tab propagation
-  if (App.data.inCompose && e.keyCode == KEY_TAB) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
-
-  if (App.autocomplete.isActive) {
-    // Just prevent propagation
-    if (~[KEY_ENTER, KEY_ESCAPE, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
-      e.preventDefault();
-      e.stopPropagation();
+    // Allways prevent tab propagation
+    if (App.data.inCompose && e.keyCode == KEY_TAB) {
+        e.preventDefault();
+        e.stopPropagation();
     }
 
-    // Escape
-    if (e.keyCode == KEY_ESCAPE) {
-      App.autocomplete.onKey(e.keyCode);
+    if (App.autocomplete.isActive) {
+        // Just prevent propagation
+        if (~[KEY_ENTER, KEY_ESCAPE, KEY_UP, KEY_DOWN].indexOf(e.keyCode)) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        // Escape
+        if (e.keyCode == KEY_ESCAPE) {
+            App.autocomplete.onKey(e.keyCode);
+        }
     }
-  }
 };
 
 App.autocomplete.onKey = function(key, e) {
-  switch(key) {
-    case KEY_TAB:
-      this.checkWord(e);
-    break;
-    case KEY_ENTER:
-      this.selectActive();
-    break;
-    case KEY_ESCAPE:
-      this.close();
-    break;
-    case KEY_UP:
-      this.changeSelection('prev');
-    break;
-    case KEY_DOWN:
-      this.changeSelection('next');
-    break;
-  }
+    switch(key) {
+        case KEY_TAB:
+            this.checkWord(e);
+            break;
+        case KEY_ENTER:
+            this.selectActive();
+            break;
+        case KEY_ESCAPE:
+            this.close();
+            break;
+        case KEY_UP:
+            this.changeSelection('prev');
+            break;
+        case KEY_DOWN:
+            this.changeSelection('next');
+            break;
+    }
 };
 
 App.autocomplete.checkWord = function(e) {
-  var cursorPosition = this.getCursorPosition(e);
-  this.cursorPosition = cursorPosition;
+    var cursorPosition = this.getCursorPosition(e);
+    this.cursorPosition = cursorPosition;
 
-  // Display loading
-  this.dropdownCreate(cursorPosition);
+    // Display loading
+    this.dropdownCreate(cursorPosition);
 
-  var word = this.getSelectedWord(cursorPosition);
+    var word = this.getSelectedWord(cursorPosition);
 
-  // Cache word
-  cursorPosition.word = word;
+    // Cache word
+    cursorPosition.word = word;
 
-  // TODO move search into background and retrieve filtered results
-  if (word.text === '') {
-    App.autocomplete.dropdownPopulate([]);
-  } else {
-    // Load all tags
-    App.settings.get('quicktexts', function(elements){
-      // Search for match
-      App.autocomplete.quicktexts = elements.filter(function(a) {
-        // TODO search for mathc in whole shorcut (now searches for match starting with the beginning)
-        return a.shortcut.indexOf(word.text) === 0;
-      });
+    // TODO move search into background and retrieve filtered results
+    if (word.text === '') {
+        App.autocomplete.dropdownPopulate([]);
+    } else {
+        // Load all tags
+        App.settings.get('quicktexts', function(elements){
+            // Search for match
+            App.autocomplete.quicktexts = elements.filter(function(a) {
+                // TODO search for mathc in whole shorcut (now searches for match starting with the beginning)
+                return a.shortcut.indexOf(word.text) === 0;
+            });
 
-      App.autocomplete.dropdownPopulate(App.autocomplete.quicktexts);
-    });
-  }
+            App.autocomplete.dropdownPopulate(App.autocomplete.quicktexts);
+        });
+    }
 };
 
 // TODO make dropdown position relative so on scrolling it will stay in right place
 App.autocomplete.dropdownCreate = function(cursorPosition) {
-  // Add loading dropdown
-  this.$dropdown = $('<ul id="qt-dropdown" class="qt-dropdown"><li class="default">Loading...</li></ul>').insertAfter(cursorPosition.elementMain);
-  this.$dropdown.css({
-    top: (cursorPosition.absolute.top + cursorPosition.absolute.height - $(window).scrollTop()) + 'px',
-    left: (cursorPosition.absolute.left + cursorPosition.absolute.width - $(window).scrollLeft()) + 'px'
-  });
+    // Add loading dropdown
+    this.$dropdown = $('<ul id="qt-dropdown" class="qt-dropdown"><li class="default">Loading...</li></ul>').insertAfter(cursorPosition.elementMain);
+    this.$dropdown.css({
+        top: (cursorPosition.absolute.top + cursorPosition.absolute.height - $(window).scrollTop()) + 'px',
+        left: (cursorPosition.absolute.left + cursorPosition.absolute.width - $(window).scrollLeft()) + 'px'
+    });
 
-  this.isActive = true;
-  this.isEmpty = true;
+    this.isActive = true;
+    this.isEmpty = true;
 
-  // Handle mouse hover and click
-  this.$dropdown.on('mouseover mousedown', 'li.qt-item', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
+    // Handle mouse hover and click
+    this.$dropdown.on('mouseover mousedown', 'li.qt-item', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
 
-    App.autocomplete.dropdownSelectItem($(this).index());
-    if (e.type === 'mousedown') {
-      App.autocomplete.selectActive();
-    }
-  });
+        App.autocomplete.dropdownSelectItem($(this).index());
+        if (e.type === 'mousedown') {
+            App.autocomplete.selectActive();
+        }
+    });
 };
 
 App.autocomplete.dropdownPopulate = function(elements) {
-  if (elements.length) {
-    var listElements = "\
-          {{#each elements}}\
-            <li class='qt-item' data-id='{{id}}'>\
-              <span class='qt-shortcut'>{{shortcut}}</span>\
-              <span class='qt-title'>{{title}}</span>\
+    if (elements.length) {
+        var listElements = "\
+        {{#each elements}}\
+        <li class='qt-item' data-id='{{id}}'>\
+            <span class='qt-shortcut'>{{shortcut}}</span>\
+            <span class='qt-title'>{{title}}</span>\
             </li>\
-          {{/each}}",
-        content = Handlebars.compile(listElements)({elements: elements});
+            {{/each}}",
+            content = Handlebars.compile(listElements)({elements: elements});
 
-    this.$dropdown.html(content);
-    this.isEmpty = false;
+        this.$dropdown.html(content);
+        this.isEmpty = false;
 
-    // Set first element active
-    this.dropdownSelectItem(0);
-  } else {
-    this.$dropdown.html('<li class="default">No results found.<br>Press Esc to close this window.<br>Press Tab to jump to Send button.</li>');
-    this.isEmpty = true;
-  }
+        // Set first element active
+        this.dropdownSelectItem(0);
+    } else {
+        this.$dropdown.html('<li class="default">No results found.<br>Press Esc to close this window.<br>Press Tab to jump to Send button.</li>');
+        this.isEmpty = true;
+    }
 };
 
 App.autocomplete.dropdownSelectItem = function(index) {
@@ -11841,141 +11840,141 @@ App.autocomplete.getCursorPosition = function(e) {
     };
     var $caret;
 
-  // Working with textarea
-  // Create a mirror element, copy textarea styles
-  // Insert text until selectionEnd
-  // Insert a virtual cursor and find its position
-  if (App.data.gmailView === 'basic html') {
-    position.element = e.target;
-    position.start = position.element.selectionStart;
-    position.end = position.element.selectionEnd;
+    // Working with textarea
+    // Create a mirror element, copy textarea styles
+    // Insert text until selectionEnd
+    // Insert a virtual cursor and find its position
+    if (App.data.gmailView === 'basic html') {
+        position.element = e.target;
+        position.start = position.element.selectionStart;
+        position.end = position.element.selectionEnd;
 
-    var $mirror = $('<div id="qt-mirror" class="qt-mirror"/>').addClass(position.element.className),
-        $source = $(position.element),
-        $sourcePosition = $source.position();
+        var $mirror = $('<div id="qt-mirror" class="qt-mirror"/>').addClass(position.element.className),
+            $source = $(position.element),
+            $sourcePosition = $source.position();
 
-    // copy all styles
-    for (var i in mirrorStyles) {
-        var style = mirrorStyles[i];
-        $mirror.css(style, $source.css(style));
+        // copy all styles
+        for (var i in mirrorStyles) {
+            var style = mirrorStyles[i];
+            $mirror.css(style, $source.css(style));
+        }
+
+        // set absolute position
+        $mirror.css({top: $sourcePosition.top + 'px', left: $sourcePosition.left + 'px'});
+
+        // copy content
+        $mirror.html($source.val().substr(0, position.end).split("\n").join('<br>'));
+        $mirror.append('<span id="qt-caret" class="qt-caret"/>');
+
+        // insert mirror
+        $mirror.insertAfter($source);
+
+        $caret = $('#qt-caret');
+        position.absolute = $caret.offset();
+        position.absolute.width = $caret.width();
+        position.absolute.height = $caret.height();
+
+        $mirror.remove();
+
+        // Working with editable div
+        // Insert a virtual cursor, find its position
+        // http://stackoverflow.com/questions/16580841/insert-text-at-caret-in-contenteditable-div
+    } else if (App.data.gmailView === 'standard') {
+        var selection = window.getSelection(),
+            range = selection.getRangeAt(0);
+
+        position.element = selection.baseNode;
+        position.start = range.startOffset;
+        position.end = range.endOffset;
+
+        range.collapse(false);   // collapse at end
+        range.deleteContents();
+
+        // Add virtual caret
+        range.insertNode(range.createContextualFragment('<span id="qt-caret"></span>'));
+
+        // Virtual caret
+        $caret = $('#qt-caret');
+
+        if ($caret.length) {
+            // Set caret back at old position
+            range = range.cloneRange();
+            range.setStartAfter($caret[0]);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            position.absolute = $caret.offset();
+            position.absolute.width = $caret.width();
+            position.absolute.height = $caret.height();
+
+            // Remove virtual caret
+            $caret.remove();
+        }
     }
 
-    // set absolute position
-    $mirror.css({top: $sourcePosition.top + 'px', left: $sourcePosition.left + 'px'});
-
-    // copy content
-    $mirror.html($source.val().substr(0, position.end).split("\n").join('<br>'));
-    $mirror.append('<span id="qt-caret" class="qt-caret"/>');
-
-    // insert mirror
-    $mirror.insertAfter($source);
-
-    $caret = $('#qt-caret');
-    position.absolute = $caret.offset();
-    position.absolute.width = $caret.width();
-    position.absolute.height = $caret.height();
-
-    $mirror.remove();
-
-  // Working with editable div
-  // Insert a virtual cursor, find its position
-  // http://stackoverflow.com/questions/16580841/insert-text-at-caret-in-contenteditable-div
-  } else if (App.data.gmailView === 'standard') {
-    var selection = window.getSelection(),
-        range = selection.getRangeAt(0);
-
-    position.element = selection.baseNode;
-    position.start = range.startOffset;
-    position.end = range.endOffset;
-
-    range.collapse(false);   // collapse at end
-    range.deleteContents();
-
-    // Add virtual caret
-    range.insertNode(range.createContextualFragment('<span id="qt-caret"></span>'));
-
-    // Virtual caret
-    $caret = $('#qt-caret');
-
-    if ($caret.length) {
-      // Set caret back at old position
-      range = range.cloneRange();
-      range.setStartAfter($caret[0]);
-      range.collapse(true);
-      selection.removeAllRanges();
-      selection.addRange(range);
-
-      position.absolute = $caret.offset();
-      position.absolute.width = $caret.width();
-      position.absolute.height = $caret.height();
-
-      // Remove virtual caret
-      $caret.remove();
-    }
-  }
-
-  return position;
+    return position;
 };
 
 App.autocomplete.selectActive = function() {
-  if (this.isActive && !this.isEmpty && this.quicktexts.length) {
-    var activeItemId = this.$dropdown.find('.active').data('id'),
-        quicktext = this.getQuicktextById(activeItemId);
+    if (this.isActive && !this.isEmpty && this.quicktexts.length) {
+        var activeItemId = this.$dropdown.find('.active').data('id'),
+            quicktext = this.getQuicktextById(activeItemId);
 
-    this.replaceWith(quicktext);
-  }
+        this.replaceWith(quicktext);
+    }
 };
 
 App.autocomplete.replaceWith = function(quicktext) {
-  var cursorPosition = this.cursorPosition,
-      word = cursorPosition.word,
-      replacement = "";
+    var cursorPosition = this.cursorPosition,
+        word = cursorPosition.word,
+        replacement = "";
 
-  if (App.data.gmailView == 'basic html') {
-    var $textarea = $(cursorPosition.element),
-        value = $textarea.val();
+    if (App.data.gmailView == 'basic html') {
+        var $textarea = $(cursorPosition.element),
+            value = $textarea.val();
 
-    replacement = Handlebars.compile(quicktext.body)(App.parser.getData(cursorPosition.elementMain));
+        replacement = Handlebars.compile(quicktext.body)(App.parser.getData(cursorPosition.elementMain));
 
-    var valueNew = value.substr(0, word.start) + replacement + value.substr(word.end),
-        cursorOffset = word.start + quicktext.body.length;
+        var valueNew = value.substr(0, word.start) + replacement + value.substr(word.end),
+            cursorOffset = word.start + quicktext.body.length;
 
-    $textarea.val(valueNew);
+        $textarea.val(valueNew);
 
-    // Set focus at the end of patch
-    $textarea.focus();
-    $textarea[0].setSelectionRange(cursorOffset, cursorOffset);
-  } else if (App.data.gmailView === 'standard') {
-    var selection = window.getSelection(),
-        range = selection.getRangeAt(0);
-    replacement = Handlebars.compile(quicktext.body)(App.parser.getData(cursorPosition.elementMain)).replace(/\n/g, '<br>');
+        // Set focus at the end of patch
+        $textarea.focus();
+        $textarea[0].setSelectionRange(cursorOffset, cursorOffset);
+    } else if (App.data.gmailView === 'standard') {
+        var selection = window.getSelection(),
+            range = selection.getRangeAt(0);
+        replacement = Handlebars.compile(quicktext.body)(App.parser.getData(cursorPosition.elementMain)).replace(/\n/g, '<br>');
 
-    range.setStart(cursorPosition.element, word.start);
-    range.setEnd(cursorPosition.element, word.end);
-    range.deleteContents();
-    range.insertNode(range.createContextualFragment(replacement + '<span id="qt-caret"></span>'));
+        range.setStart(cursorPosition.element, word.start);
+        range.setEnd(cursorPosition.element, word.end);
+        range.deleteContents();
+        range.insertNode(range.createContextualFragment(replacement + '<span id="qt-caret"></span>'));
 
-    // Virtual caret
-    // Used to set cursor position in right place
-    // TODO find a better method to do that
-    $caret = $('#qt-caret');
+        // Virtual caret
+        // Used to set cursor position in right place
+        // TODO find a better method to do that
+        $caret = $('#qt-caret');
 
-    if ($caret.length) {
-      // Set caret back at old position
-      range = range.cloneRange();
-      range.setStartAfter($caret[0]);
-      range.collapse(true);
-      selection.removeAllRanges();
-      selection.addRange(range);
+        if ($caret.length) {
+            // Set caret back at old position
+            range = range.cloneRange();
+            range.setStartAfter($caret[0]);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
 
-      // Remove virtual caret
-      $caret.remove();
+            // Remove virtual caret
+            $caret.remove();
+        }
     }
-  }
 
-  // updates stats
-  App.settings.stats('words', quicktext.body.split(" ").length, function(){});
-  App.autocomplete.close();
+    // updates stats
+    App.settings.stats('words', quicktext.body.split(" ").length, function(){});
+    App.autocomplete.close();
 };
 
 // TODO should request background
