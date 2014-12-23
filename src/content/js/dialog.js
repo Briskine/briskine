ec1dbbc49807f690131a8969290e21c82b5ddd57
Alex Plugaru <alex@plugaru.org>
2014-12-23 17:31:42 +0100

    Don't remove the currently selected word if it doesn't match the shortcut. Fixes #81

--- src/content/js/dialog.js
+++ src/content/js/dialog.js
@@ -5,7 +5,6 @@
 
 PubSub.subscribe('focus', function (action, element) {
     if (action === 'off') {
-        console.log(action, element);
         if (element === null) {
             App.autocomplete.dialog.close();
         } else if ($(element).attr('class') !== $(App.autocomplete.dialog.searchSelector).attr('class')) {
@@ -69,7 +68,7 @@ App.autocomplete.dialog = {
         var dialog = $(this.template);
         container.append(dialog);
 
-        //HACK: set z-index to auto to a parent, otherwise the autocomplete
+        //Gmail HACK: set z-index to auto to a parent, otherwise the autocomplete
         //      dropdown will not be displayed with the correct stacking
         dialog.parents('.qz').css('z-index', 'auto');
 
