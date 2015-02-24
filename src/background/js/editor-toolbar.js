a5a7f266e0c5bff686dd3677e47b7d70f3f73379
Alex Plugaru <alex@plugaru.org>
2015-02-24 12:40:54 +0100

    Added help link for editor variables. Removed lists for now.

--- src/background/js/editor-toolbar.js
+++ src/background/js/editor-toolbar.js
@@ -3,9 +3,9 @@
 
 gqApp.config(function ($provide) {
 
-    $provide.decorator('taOptions', [ 'taRegisterTool', '$delegate', '$timeout', 
+    $provide.decorator('taOptions', [ 'taRegisterTool', '$delegate', '$timeout',
     function(taRegisterTool, taOptions, $timeout) {
-        
+
         // place focus at the end of a contenteditable
         var focusEnd = function(el) {
 
@@ -22,7 +22,7 @@ gqApp.config(function ($provide) {
         };
 
         var insertHtml = function(qtvar) {
-            
+
             // we can't wrap the variable in a html tag
             // because it will cause issues when adding multiple vars
             // one after the other.
@@ -31,32 +31,32 @@ gqApp.config(function ($provide) {
             // inside (!) the br tag.
             var template = '%var%';
             template =  template.replace(/%var%/g, qtvar);
-            
+
             this.$editor().wrapSelection('inserthtml', template);
         };
-        
+
         var qtMethods = {};
-    
+
         qtMethods.InsertSubject = function() {
             insertHtml.call(this, '{{subject}}');
         };
-        
+
         qtMethods.InsertFirstFromName = function() {
-            insertHtml.call(this, '{{from.0.name}}');
+            insertHtml.call(this, '{{from.0.first_name}}');
         };
-        
+
         qtMethods.InsertFirstFromEmail = function() {
             insertHtml.call(this, '{{from.0.email}}');
         };
-        
+
         qtMethods.InsertFirstToName = function() {
-            insertHtml.call(this, '{{to.0.name}}');
+            insertHtml.call(this, '{{to.0.first_name}}');
         };
-        
+
         qtMethods.InsertFirstToEmail = function() {
             insertHtml.call(this, '{{to.0.email}}');
         };
-        
+
         var listTemplate = '';
         listTemplate += '{{#each %list%}}<br>';
         listTemplate += 'Name {{name}}<br>';
@@ -64,55 +64,53 @@ gqApp.config(function ($provide) {
         listTemplate += 'Last name {{last_name}}<br>';
         listTemplate += 'Email {{email}}<br>';
         listTemplate += '{{/each}}';
-        
+
         qtMethods.InsertFromList = function() {
             var template = listTemplate.replace(/%list%/g, 'from');
-            
+
             insertHtml.call(this, template);
         };
-        
+
         qtMethods.InsertToList = function() {
             var template = listTemplate.replace(/%list%/g, 'to');
-            
+
             insertHtml.call(this, template);
         };
-        
+
         qtMethods.InsertCcList = function() {
             var template = listTemplate.replace(/%list%/g, 'cc');
-            
+
             insertHtml.call(this, template);
         };
-        
+
         qtMethods.InsertBccList = function() {
             var template = listTemplate.replace(/%list%/g, 'bcc');
-            
+
             insertHtml.call(this, template);
         };
 
         var dropMenuTemplate = '<div class="dropdown insert-var-container">';
         dropMenuTemplate += '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" ng-mouseup="focusHack()">';
-        dropMenuTemplate += 'Insert variable<span class="caret"></span>';
+        dropMenuTemplate += 'Insert Variable<span class="caret"></span>';
         dropMenuTemplate += '</button>';
         dropMenuTemplate += '<ul class="dropdown-menu">';
-        dropMenuTemplate += '<li><a ng-click="InsertSubject()">Subject</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertFirstFromName()">First FROM Name</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertFirstFromEmail()">First FROM Email</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertFirstToName()">First TO Name</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertFirstToEmail()">First TO Email</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertFromList()">FROM List</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertToList()">TO List</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertCcList()">CC List</a></li>';
-        dropMenuTemplate += '<li><a ng-click="InsertBccList()">BCC List</a></li>';
+        dropMenuTemplate += '<li><a ng-click="InsertFirstToName()" title="First name of the person in the \'To\' field"><strong>To:</strong> First Name</a></li>';
+        dropMenuTemplate += '<li><a ng-click="InsertFirstToEmail()" title="Email address of the person in the \'To\' field"><strong>To:</strong> Email</a></li>';
+        dropMenuTemplate += '<li><a ng-click="InsertFirstFromName()" title="First name of the person in the \'From\' field"><strong>From:</strong> First Name</a></li>';
+        dropMenuTemplate += '<li><a ng-click="InsertFirstFromEmail()" title="Email address of the person in the \'From\' field"><strong>From:</strong> Email</a></li>';
+        dropMenuTemplate += '<li><a ng-click="InsertSubject()" title="The subject of the e-mail"><strong>Subject</strong></a></li>';
+        dropMenuTemplate += '<li class="divider"></li>';
+        dropMenuTemplate += '<li><a class="help-block text-muted small" href="http://help.gorgias.io/en/latest/src/04-templates.html" rel="external" target="_blank">Learn more about template variables <i class="fa fa-external-link"></i></a></li>';
         dropMenuTemplate += '</ul>';
         dropMenuTemplate += '</div>';
-        
+
         // register the tool with textAngular
         taRegisterTool('insertVariable', {
             display: dropMenuTemplate,
             disabled: function() {
-                
+
                 // runs as an init function
-                
+
                 // hack to get around the errors thrown by textAngular
                 // because it didn't get to store a pointer to the editor,
                 // because it's not focused.
@@ -142,14 +140,14 @@ gqApp.config(function ($provide) {
 
                     }
                 };
-                
+
                 var self = this;
-                
+
                 // insert all qtMethods into the scope
                 Object.keys(qtMethods).forEach(function(key) {
                     self[key] = qtMethods[key];
                 });
-                
+
                 this.isDisabled = function() {
                     return false;
                 };
@@ -157,11 +155,11 @@ gqApp.config(function ($provide) {
             },
             action: function(){}
         });
-        
-        return taOptions;
-    
-    }]);
-    
 
-    
+        return taOptions;
+
+    }]);
+
+
+
 });
