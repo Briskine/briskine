be33a4c90a63d3f36d2b22189f0399c282ac5aff
Ionut Colceriu <contact@ghinda.net>
2015-03-06 19:41:22 +0200

    Cleanup some of the refactored positioning code. Found a new issue with the dialog being wrongly positioned when no `word` is selected, and/sometimes, when the contenteditable has overflow: auto/scroll, and is scrolled.

--- src/content/js/autocomplete.js
+++ src/content/js/autocomplete.js
@@ -133,13 +133,22 @@ App.autocomplete.getCursorPosition = function (element) {
         // Virtual caret
         $caret = $('#qt-caret');
 
+        // TODO caret placed at the wrong position when no text is selected
+        // and the container si a scroller contenteditable
+        console.log(position.word);
+
         if ($caret.length) {
+
+            console.log($caret);
+
             position.absolute = $caret.offset();
             position.absolute.width = $caret.width();
             position.absolute.height = $caret.height();
 
+            console.log(position.absolute);
+
             // Remove virtual caret
-            $caret.remove();
+            //$caret.remove();
         }
 
     } else {
