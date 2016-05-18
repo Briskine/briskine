4c62cb51d39dec1144d1a832ce0cc070d2fbff98
Ticruz <thib.duchene@gmail.com>
2016-05-18 15:56:17 +0200

    bugfix issue#183

--- src/background/js/controllers/includes/template_form.js
+++ src/background/js/controllers/includes/template_form.js
@@ -60,6 +60,7 @@ gApp.controller('TemplateFormCtrl',
         }
         self.removeAttachment = function(index) {
            self.selectedTemplate.attachments.splice(index, 1);
+           if(self.selectedTemplate.attachments.length === 0) self.selectedTemplate.attachments = null;
            $rootScope.$broadcast('reload');
         }
         self.onPickerClicked = gDrivePickerService.onPickerClicked;
@@ -168,7 +169,7 @@ gApp.controller('TemplateFormCtrl',
                         'title': '',
                         'tags': '',
                         'body': '',
-                        'attachments': []
+                        'attachments': null
                     };
 
                     id = id ? id : $routeParams.id;
