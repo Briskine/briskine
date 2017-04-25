67e8e22a675241d2082a6a49972bba5c3ab6b3ed
Alex Plugaru <alex@gorgias.io>
2017-04-25 15:21:57 -0700

    Added domain helper

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -140,3 +140,31 @@ var fuzzySearch = function (list, text, opts) {
     var fuse = new Fuse(list, options);
     return fuse.search(text);
 };
+
+function underscored(str) {
+    return str.replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase().trim();
+}
+
+function titleize(str) {
+    return str.toLowerCase().replace(/(?:^|\s|-)\S/g, function (c) {
+        return c.toUpperCase();
+    });
+}
+
+function humanize(str) {
+    return titleize(underscored(str).replace(/_id$/, '').replace(/_/g, ' '));
+}
+
+Handlebars.registerHelper('domain', function (text, options) {
+    if (!text || typeof text !== 'string') {
+        return text
+    }
+
+    var split = text.split('@'); //contact@AWESOME-sweet-bakery.co.uk
+    if (split.length !== 2) {
+        return text
+    }
+    var tld = split[1]; // AWESOME-sweet-bakery.co.uk
+    var domain = tld.split('.')[0]; // AWESOME-sweet-bakery
+    return humanize(domain); // Awesome Sweet Bakery
+});
