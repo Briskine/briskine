8287b8ba5da68ecb92c5942ed2b574e0fe23a0e2
Alex Plugaru <alex@gorgias.io>
2018-08-03 16:03:35 -0700

    Removed tracking

--- src/background/js/utils/ga.js
+++
@@ -1,9 +0,0 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-111444694-1', 'auto');
-ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
-ga('require', 'displayfeatures');
-ga('send', 'pageview', '/options.html');
-ga('require', 'Clearbit', {"mapping":{"companyName":"dimension1","companyDomain":"dimension2","companyTags":"dimension3","companySector":"dimension4","companyIndustryGroup":"dimension5","companyIndustry":"dimension6","companySubIndustry":"dimension7","companyType":"dimension8","companyEmployeesRange":"dimension9","companyAlexaRank":"dimension10","companyCity":"dimension11","companyState":"dimension12","companyCountry":"dimension13","companyFunding":"dimension14","companyTech":"dimension15"}});
