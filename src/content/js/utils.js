2ccc9aa59ec04722d62039499e743943a0d4eb89
akshaycj <akshaycj999@gmail.com>
2018-11-05 20:54:27 +0530

    Moved getCapsonBegin to Utils

--- src/content/js/utils.js
+++ src/content/js/utils.js
@@ -4,10 +4,10 @@
 
 App.utils = (function() {
     // split full name by last space.
-    var splitFullName = function (fullname) {
-        fullname = fullname || '';
+    var splitFullName = function(fullname) {
+        fullname = fullname || "";
 
-        var lastSpaceIndex = fullname.lastIndexOf(' ');
+        var lastSpaceIndex = fullname.lastIndexOf(" ");
         if (lastSpaceIndex < 1) {
             lastSpaceIndex = fullname.length;
         }
@@ -20,20 +20,20 @@ App.utils = (function() {
 
     // extracts name and email from google sign-out title string.
     // title = Google Account: User Name (user@email.net)
-    var parseUserDetails = function (title) {
+    var parseUserDetails = function(title) {
         var details = {
-            email: '',
-            name: ''
+            email: "",
+            name: ""
         };
-        var sep = ':';
+        var sep = ":";
 
         if (title && title.indexOf(sep) !== -1) {
             var prefix = title.split(sep)[0] + sep;
-            details.name = title.replace(prefix, '').trim();
+            details.name = title.replace(prefix, "").trim();
         }
 
         if (details.name) {
-            var openBracket = details.name.lastIndexOf('(');
+            var openBracket = details.name.lastIndexOf("(");
             // in case of no brackets
             if (openBracket === -1) {
                 openBracket = details.name.length;
@@ -47,8 +47,15 @@ App.utils = (function() {
         return jQuery.extend(details, splitFullName(details.name));
     };
 
+    //Capitalize first char of a string
+    var getCapsonBegin = function(s) {
+        if (typeof s !== "string") return ""; //Check whether params is a string, and returns empty on non-string
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    };
+
     return {
         splitFullName: splitFullName,
-        parseUserDetails: parseUserDetails
+        parseUserDetails: parseUserDetails,
+        getCapsonBegin: getCapsonBegin
     };
-}());
+})();
