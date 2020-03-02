cf470cfa28bf52d37d75e73fbeb6e18ce708bacd
Ionut Colceriu <contact@ghinda.net>
2020-03-02 17:10:37 +0200

    Point api urls to new domain.

--- src/background/js/config.js
+++ src/background/js/config.js
@@ -3,7 +3,7 @@ var Config = {
     baseURL: 'http://localhost:5555/',
     apiBaseURL: 'http://localhost:5555/api/1/',
     websiteUrl: 'http://localhost:4000',
-    functionsUrl: 'http://localhost:5000/gorgias-templates-development/us-central1'
+    functionsUrl: 'http://localhost:5000'
 };
 
 // firebase staging
@@ -11,7 +11,7 @@ if (ENV === 'staging') {
     Config = Object.assign(Config, {
         baseURL: 'https://chrome.gorgias.io/',
         apiBaseURL: 'https://chrome.gorgias.io/api/1/',
-        functionsUrl: 'https://us-central1-gorgias-templates-staging.cloudfunctions.net'
+        functionsUrl: 'https://staging.gorgiastemplates.com'
     });
 }
 
@@ -20,7 +20,7 @@ if (ENV === 'production') {
         baseURL: 'https://chrome.gorgias.io/',
         apiBaseURL: 'https://chrome.gorgias.io/api/1/',
         websiteUrl: 'https://templates.gorgias.io',
-        functionsUrl: 'https://us-central1-gorgias-templates-production.cloudfunctions.net'
+        functionsUrl: 'https://app.gorgiastemplates.com'
     });
 }
 
