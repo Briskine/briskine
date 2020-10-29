b7aa764bf0b967a45543e20676ca4072ca21ebfe
Ionut Colceriu <contact@ghinda.net>
2020-10-29 12:03:24 +0200

    Get the Gmail from vars from the user details tooltip, instead of the aria attribute. Fixes issues with missing from data in non-google-apps accounts.

--- src/content/js/plugins/gmail.js
+++ src/content/js/plugins/gmail.js
@@ -50,18 +50,13 @@ function getData (params) {
         subject = '';
 
     if (isContentEditable(params.element)) {
-        var title = $('[data-eqid]').attr('aria-label');
-        var fromString = '';
-        if (typeof title === 'string') {
-            // the initial string is: Google Account: Gorgias Templates Support (support@gorgiastemplates.com)
-            var parts = title.trim().split(':');
-            if (parts.length && parts.length === 2) {
-                fromString = parts[1];
-            }
-        } else {
-            // old format
-            fromString = $('.gb_vb').text() + '<' + $('.gb_xb').text() + '>';
-        }
+        const userInfoSelector = '.gb_qe';
+        const $fullName = document.querySelector(`${userInfoSelector} *:nth-last-child(2)`);
+        const $email = document.querySelector(`${userInfoSelector} *:last-child`);
+
+        const fullNameText = $fullName ? $fullName.innerText : '';
+        const emailText = $email ? $email.innerText : '';
+        const fromString = `${fullNameText} <${emailText}>`;
 
         from.push(parseString(fromString));
 
