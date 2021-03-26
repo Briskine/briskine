9d268a85023e69644c75cc2117325fd2aeabc1f6
Ionut Colceriu <contact@ghinda.net>
2021-03-26 12:45:44 +0200

    Use html-to-text instead of our custom html to text converter. Fixes issues with newlines not being added correctly for block nodes and br tags.

--- src/content/js/utils/plain-text.spec.js
+++ src/content/js/utils/plain-text.spec.js
@@ -4,7 +4,27 @@ import {htmlToText} from './plain-text';
 
 describe('htmlToText', () => {
     it('should convert html to text', () => {
-        expect(htmlToText('<div>first-line<div>second-line</div></div>')).equal('first-line\nsecond-line');
+        expect(htmlToText('<div>first-line<div>second-line</div>third-line</div>')).equal('first-line\nsecond-line\nthird-line');
+    });
+
+    it('should add end newlines to block nodes', () => {
+        expect(htmlToText('<div><div>first</div>second</div>')).equal('first\nsecond');
+    });
+
+    it('should add correct newlines to successive block nodes', () => {
+        expect(htmlToText('<div>first</div><div>second</div>')).equal('first\nsecond');
+    });
+
+    it('should add newlines for br and divs', () => {
+        expect(htmlToText('<div>first<br></div><br><div>second</div><br>third')).equal('first\n\n\nsecond\n\nthird');
+    });
+
+    it('should add newlines for br inside inline nodes', () => {
+        expect(htmlToText('<span>first<br></span>second')).equal('first\nsecond');
+    });
+
+    it('should remove extra newlines from html', () => {
+        expect(htmlToText('<div>first</div>\n\n<div>second</div>')).equal('first\nsecond');
     });
 
     it('should keep plain text links intact', () => {
@@ -17,11 +37,11 @@ describe('htmlToText', () => {
     });
 
     it('should decorate lists', () => {
-        expect(htmlToText('<ul><li>one</li><li>two</li</ul>')).equal('- one\n- two');
+        expect(htmlToText('<ul><li>one</li><li>two</li</ul>')).equal(' * one\n * two');
     });
 
     it('should decorate links', () => {
-        expect(htmlToText('<a href="https://gorgiastemplates.com/">gorgiastemplates</a>')).equal('gorgiastemplates (https://gorgiastemplates.com/)');
+        expect(htmlToText('<a href="https://gorgiastemplates.com/">gorgiastemplates</a>')).equal('gorgiastemplates [https://gorgiastemplates.com/]');
     });
 
     it('should decorate links only with different urls', () => {
@@ -29,12 +49,10 @@ describe('htmlToText', () => {
     });
 
     it('should decorate images', () => {
-        expect(htmlToText('<img src="https://gorgiastemplates.com/image.jpg" alt="image alt">')).equal('[image alt https://gorgiastemplates.com/image.jpg]');
+        expect(htmlToText('<img src="https://gorgiastemplates.com/image.jpg" alt="image alt">')).equal('image alt [https://gorgiastemplates.com/image.jpg]');
     });
 
-    it('should keep code blocks as code', () => {
-        const code = '<div></div><div><textarea></textarea></div>';
-        expect(htmlToText(`<code>${code}</code>`)).equal(code);
+    it('should add newlines for br tags', () => {
+        expect(htmlToText('first<br /><br />second<br><br>third')).equal('first\n\nsecond\n\nthird');
     });
-
 });
