9e9b11b0ce19fdc1b1dfd2d42c5d1b055a5e38ba
Ionut Colceriu <contact@ghinda.net>
2021-06-30 12:57:54 +0300

    Position the bubble by moving it to the offset parent. Positioning tweaks required if the parent is a table element.

--- src/content/js/bubble.js
+++ src/content/js/bubble.js
@@ -4,18 +4,25 @@
  */
 
 customElements.define(
-    'g-bubble',
+    'b-bubble',
     class extends HTMLElement {
         constructor() {
             super();
 
             const template = `
                 <style>
-                    .g-button {
+                    .b-bubble {
+                        background: red;
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                    }
+
+                    body {
                         background: red;
                     }
                 </style>
-                <button type="button" class="g-button">
+                <button type="button" class="b-bubble">
                     gbutton
                 </button>
             `
@@ -28,7 +35,10 @@ customElements.define(
     }
 );
 
+let bubbleInstance = null;
+
 export function setup () {
+    // TODO check settings and contenteditable before setting up the bubble
     if (bubbleEnabled()) {
         return create();
     }
@@ -50,10 +60,20 @@ function create () {
     // when the textfield is focused, move it close to it.
     // use shadow dom for the content and the styles.
 
-    const bubble = document.createElement('g-bubble');
-    document.documentElement.appendChild(bubble);
+    bubbleInstance = document.createElement('b-bubble');
+    document.documentElement.appendChild(bubbleInstance);
 
-    console.log('append bubble', bubble);
+    console.log('append bubble', bubbleInstance);
+
+    document.body.addEventListener('focusin', (e) => {
+        return showBubble(e);
+    });
+
+    document.body.addEventListener('focusout', (e) => {
+        return hideBubble();
+    });
+
+
 
 //     var container = $('body');
 //
@@ -137,10 +157,131 @@ function create () {
 //     });
 };
 
+function showQaForElement (elem) {
+    var show = false;
+
+    // if the element is not a textarea
+    // input[type=text] or contenteditable
+    if (elem.matches('textarea, input[type=text], [contenteditable]')) {
+        show = true;
+    }
+
+    // if the quick access button is focused/clicked
+    if (elem.className.indexOf('gorgias-qa-btn') !== -1) {
+        show = false;
+    }
+
+    // if the dialog search field is focused
+    if (elem.className.indexOf('qt-dropdown-search') !== -1) {
+        show = false;
+    }
+
+    // check if the element is big enough
+    // to only show the qa button for large textfields
+    if (show === true) {
+        var metrics = elem.getBoundingClientRect();
+        if (metrics.width < 100 || metrics.height < 30) {
+            show = false;
+        }
+    }
+
+    return show;
+}
+
+function showBubble (e) {
+    var textfield = e.target;
+
+    // only show it for valid elements
+    if (!showQaForElement(textfield)) {
+        return false;
+    }
+
+    console.log('show for', e.target);
+
+    console.log('offsetParent', e.target.offsetParent, e.target.offsetTop, e.target.offsetLeft);
+
+    const offsetParent = e.target.offsetParent;
+    if (offsetParent) {
+        const offsetStyles = window.getComputedStyle(offsetParent);
+        // in case the offsetParent is a unpositioned table element (td, th, table)
+        // make it relative, for the button to have the correct positioning.
+        if (offsetStyles.position === 'static') {
+            offsetParent.style.position = 'relative';
+        }
+
+        offsetParent.appendChild(bubbleInstance);
+    }
+
+//     if (relativeParent) {
+//         // TODO only if we have relativeParent
+//         relativeParent.appendChild(bubbleInstance);
+//     }
+
+    // TODO use settings on create, not on show
+//     store.getSettings({
+//         key: 'settings'
+//     }).then((settings) => {
+//         if (settings.qaBtn && settings.qaBtn.enabled === false) {
+//             return;
+//         }
+//
+//         $('body').addClass('gorgias-show-qa-btn');
+//
+//         dialog.prevFocus = textfield;
+//
+//         var qaBtn = dialog.qaBtn.get(0);
+//
+//         // padding from the top-right corner of the textfield
+//         var padding = 10;
+//
+//         // positioning the quick-action button.
+//         // Gmail is custom made
+//         // TODO use the general positioning method for Gmail
+//         if (window.location.origin === "https://mail.google.com") {
+//             var gmailHook = $(textfield).closest('td');
+//             if (gmailHook.length) {
+//                 $(qaBtn).css({
+//                     'top': padding + "px",
+//                     'right': padding + "px",
+//                     'left': 'initial'
+//                 });
+//                 qaBtn.remove();
+//                 gmailHook.append(qaBtn);
+//
+//                 // First time a user uses our extension, we show it and then hide it
+//                 if (settings.qaBtn && settings.qaBtn.hasOwnProperty('shownPostInstall')) {
+//                     if (!settings.qaBtn.shownPostInstall) {
+//                         $(qaBtn).trigger('mouseup');
+//                         settings.qaBtn.shownPostInstall = true;
+//                         store.setSettings({
+//                             key: 'settings',
+//                             val: settings
+//                         });
+//                     }
+//                 }
+//                 return;
+//             }
+//         } else {
+//             activeTextfield = textfield;
+//             setQaBtnPosition();
+//             window.addEventListener('scroll', setQaBtnPosition, true);
+//         }
+//
+//     });
+}
+
+function hideBubble () {
+    document.documentElement.appendChild(bubbleInstance);
+}
+
 export function enableBubble () {
-    document.body.dataset.gorgiasButton = 'true';
+    document.body.dataset.briskineButton = 'true';
 };
 
 function bubbleEnabled () {
-    return !!document.body.dataset.gorgiasButton;
+    // gorgiasButton for legacy enterprise support
+    const attrs = [ 'gorgiasButton', 'briskineButton' ];
+    return attrs.some((a) => {
+        return !!document.body.dataset[a];
+    });
 };
