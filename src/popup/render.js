d5a4d6fc68b611b5062d28723aa9b80a5bd791c1
Ionut Colceriu <contact@ghinda.net>
2021-10-12 14:21:00 +0300

    Use nanomorph instead of morphdom.

--- src/popup/render.js
+++ src/popup/render.js
@@ -1,4 +1,4 @@
-import morphdom from 'morphdom'
+import nanomorph from 'nanomorph'
 
 export default function render (fromElement, toElement, options = {}) {
     options.childrenOnly = true
@@ -9,5 +9,5 @@ export default function render (fromElement, toElement, options = {}) {
         toElement = fragment
     }
 
-    return morphdom(fromElement, toElement, options)
+    return nanomorph(fromElement, toElement, options)
 }
