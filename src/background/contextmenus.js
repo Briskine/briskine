3fa94d03adfb99481c12b74c110f738987ff8787
Ionut Colceriu <contact@ghinda.net>
2021-10-20 12:17:11 +0300

    Fix issues with the context menu opening the new tab twice, after update.

--- src/background/contextmenus.js
+++ src/background/contextmenus.js
@@ -2,23 +2,26 @@ import browser from 'webextension-polyfill'
 
 import Config from '../config'
 
+const saveAsTemplate = 'saveAsTemplate'
+
+function clickContextMenu (info) {
+  if (info.menuItemId === saveAsTemplate) {
+    const body = encodeURIComponent(info.selectionText)
+    browser.tabs.create({
+      url: `${Config.functionsUrl}/#/list?id=new&body=${body}`
+    })
+  }
+}
+
 export default function setupContextMenus () {
   browser.contextMenus.removeAll()
-
-  const saveAsTemplate = 'saveAsTemplate'
-
-  browser.contextMenus.create({
-    title: 'Save "%s" as a template',
-    contexts: ['selection'],
-    id: saveAsTemplate
-  })
-
-  browser.contextMenus.onClicked.addListener((info) => {
-    if (info.menuItemId === saveAsTemplate) {
-      const body = encodeURIComponent(info.selectionText)
-      browser.tabs.create({
-        url: `${Config.functionsUrl}/#/list?id=new&body=${body}`
+    .then(() => {
+      browser.contextMenus.create({
+        title: 'Save "%s" as a template',
+        contexts: ['selection'],
+        id: saveAsTemplate
       })
-    }
-  })
+
+      browser.contextMenus.onClicked.addListener(clickContextMenu)
+    })
 }
