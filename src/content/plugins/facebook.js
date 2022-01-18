28419eb3734edd89b391a0253ab3af214daf316a
Ionut Colceriu <contact@ghinda.net>
2022-01-18 15:10:51 +0200

    Fix to variable on facebook.

--- src/content/plugins/facebook.js
+++ src/content/plugins/facebook.js
@@ -5,97 +5,54 @@ import {parseTemplate} from '../utils.js'
 import {createContact} from '../utils/data-parse.js'
 import {insertLexicalText} from '../utils/editor-lexical.js'
 
-var parseName = function(name) {
-    name = name.trim();
-
-    var firstSpace = name.indexOf(' ');
-
-    if(firstSpace === -1) {
-        firstSpace = name.length;
-    }
-
-    var first_name = name.substring(0, firstSpace);
-    var last_name = name.substring(firstSpace + 1, name.length);
-
-    return {
-        first_name: first_name,
-        last_name: last_name
-    };
-};
-
-var fromDetails = []
 function getFromDetails () {
-  if (!fromDetails.length) {
-    var objectMatch = new RegExp('"NAME":.?".*?"')
-    var plainUserObject = ''
-    // get full name from inline script
-    Array.from(document.scripts).some((script) => {
-      var match = (script.textContent || '').match(objectMatch)
-      if (!script.src && match) {
-        plainUserObject = match[0] || ''
-        return true
-      }
+  var objectMatch = new RegExp('"NAME":.?".*?"')
+  var plainUserObject = ''
+  // get full name from inline script
+  Array.from(document.scripts).some((script) => {
+    var match = (script.textContent || '').match(objectMatch)
+    if (!script.src && match) {
+      plainUserObject = match[0] || ''
+      return true
+    }
+  })
+
+  var fromName = ''
+  try {
+    var parsedUserObject = JSON.parse(`{${plainUserObject}}`)
+    fromName = parsedUserObject.NAME || '';
+  } catch(err) {}
+
+  return createContact({
+      name: fromName,
+      email: ''
     })
-
-    var fromName = ''
-    try {
-      var parsedUserObject = JSON.parse(`{${plainUserObject}}`)
-      fromName = parsedUserObject.NAME || '';
-    } catch(err) {}
-
-    fromDetails = createContact({
-        name: fromName,
-        email: ''
-      })
-  }
-
-  return fromDetails
 }
 
-var getToDetails = function () {
-    var singleToContainer = '._1jt6';
-    // single recipient.
-    // link on messenger.com,
-    // hovercard on facebook.com.
-    var $to = document.querySelector(`
-        ${singleToContainer} a[href*="//www.facebook.com/"],
-        ${singleToContainer} a[data-hovercard]
-    `);
-    if ($to) {
-        var singleToName = ($to.innerText || '').trim();
-        return [
-            Object.assign({
-                name: singleToName,
-                first_name: '',
-                last_name: '',
-                email: ''
-            }, parseName(singleToName))
-        ];
-    }
+function getToDetails (editor) {
+  // role=main for messenger
+  // data-testid for message popup on facebook
+  const parentSelector = `[role=main], [data-testid]`
+  const $to = editor.closest(parentSelector).querySelector('img[alt]')
 
-    // group
-    var $recipients = document.querySelectorAll('._364g');
-    if ($recipients.length) {
-        return Array.from($recipients).map(($person) => {
-            var toName = ($person.innerText || '').trim();
-            return Object.assign({
-                name: toName,
-                first_name: '',
-                last_name: '',
-                email: ''
-            }, parseName(toName));
-        });
-    }
+  if ($to) {
+    return [
+      createContact({
+        name: $to.getAttribute('alt'),
+        email: ''
+      })
+    ]
+  }
 
-    return [];
-};
+  return []
+}
 
 // get all required data from the dom
-function getData () {
-    return {
-        from: getFromDetails(),
-        to: getToDetails()
-    };
+function getData (params) {
+  return {
+    from: getFromDetails(),
+    to: getToDetails(params.element)
+  }
 }
 
 var activeCache = null;
