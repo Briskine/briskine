b43c2cca8cd35f0a384714944b5c3001e98d2a35
Ionut Colceriu <contact@ghinda.net>
2022-02-22 15:33:01 +0200

    Webpack config formatting.

--- webpack.config.js
+++ webpack.config.js
@@ -24,122 +24,122 @@ const defaultFirebaseConfig = {
 const devPath = path.resolve('ext')
 const productionPath = path.resolve('build')
 
-const safariManifestDescription = 'Write emails faster! Increase your productivity with templates and shortcuts on Gmail, Outlook, or LinkedIn.';
+const safariManifestDescription = 'Write emails faster! Increase your productivity with templates and shortcuts on Gmail, Outlook, or LinkedIn.'
 
 function generateManifest (safari) {
-    let updatedManifestFile = Object.assign({}, manifestFile);
-    // get version from package
-    updatedManifestFile.version = packageFile.version;
+  let updatedManifestFile = Object.assign({}, manifestFile)
+  // get version from package
+  updatedManifestFile.version = packageFile.version
 
-    // safari manifest
-    if (safari) {
-        updatedManifestFile.description = safariManifestDescription;
-    }
+  // safari manifest
+  if (safari) {
+    updatedManifestFile.description = safariManifestDescription
+  }
 
-    return new CopyWebpackPlugin({
-        patterns: [
-            {
-                from: './src/manifest.json',
-                transform: function () {
-                    return Buffer.from(JSON.stringify(updatedManifestFile));
-                }
-            }
-        ]
-    });
+  return new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: './src/manifest.json',
+        transform: function () {
+          return Buffer.from(JSON.stringify(updatedManifestFile))
+        }
+      }
+    ]
+  })
 }
 
 class ZipPlugin {
-    constructor(options) {
-        this.options = options;
-    }
-    apply(compiler) {
-        compiler.hooks.done.tapAsync('ZipPlugin', (params, callback) => {
-            const output = fs.createWriteStream(this.options.output);
-            const zipArchive = archiver('zip');
-            output.on('close', callback);
-            zipArchive.pipe(output);
-            zipArchive.directory(this.options.entry, false);
-            zipArchive.finalize();
-        });
-    }
+  constructor(options) {
+    this.options = options
+  }
+  apply(compiler) {
+    compiler.hooks.done.tapAsync('ZipPlugin', (params, callback) => {
+      const output = fs.createWriteStream(this.options.output)
+      const zipArchive = archiver('zip')
+      output.on('close', callback)
+      zipArchive.pipe(output)
+      zipArchive.directory(this.options.entry, false)
+      zipArchive.finalize()
+    })
+  }
 }
 
-function extensionConfig (env, safari = false, firebaseConfig) {
-    const plugins = [
-        generateManifest(safari),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/popup/popup.html', to: 'popup/' },
-                { from: 'src/pages/', to: 'pages/' },
-                { from: 'src/icons/', to: 'icons/' },
-                { from: 'LICENSE', to: '' }
-            ]
-        }),
-        new webpack.DefinePlugin({
-            ENV: JSON.stringify(env),
-            REGISTER_DISABLED: safari,
-            FIREBASE_CONFIG: JSON.stringify(firebaseConfig)
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]/[name].css'
-        }),
-        new PurgecssPlugin({
-          paths: glob.sync('src/**/*',  { nodir: true })
-        })
-    ];
+function extensionConfig (env, safari = false, firebaseConfig = {}) {
+  const plugins = [
+    generateManifest(safari),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/popup/popup.html', to: 'popup/' },
+        { from: 'src/pages/', to: 'pages/' },
+        { from: 'src/icons/', to: 'icons/' },
+        { from: 'LICENSE', to: '' }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(env),
+      REGISTER_DISABLED: safari,
+      FIREBASE_CONFIG: JSON.stringify(firebaseConfig)
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]/[name].css'
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync('src/**/*',  { nodir: true })
+    })
+  ]
 
-    if (env === 'production') {
-        const zipFilename = `${packageFile.name}-${packageFile.version}.zip`;
-        const zipPath = path.join(productionPath, zipFilename);
-        plugins.push(
-            new ZipPlugin({
-                entry: devPath,
-                output: zipPath
-            })
-        );
-    }
+  if (env === 'production') {
+    const zipFilename = `${packageFile.name}-${packageFile.version}.zip`
+    const zipPath = path.join(productionPath, zipFilename)
+    plugins.push(
+      new ZipPlugin({
+        entry: devPath,
+        output: zipPath
+      })
+    )
+  }
 
-    return {
-        entry: {
-            background: './src/background/background.js',
-            popup: './src/popup/popup.js',
-            content: './src/content/index.js'
-        },
-        output: {
-            path: path.resolve(devPath),
-            filename: '[name]/[name].js',
-            clean: true
-        },
-        plugins: plugins,
-        module: {
-            rules: [
-                {
-                    test: /\.(css)$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader'
-                    ],
-                },
-                {
-                    test: /\.(png|svg)$/,
-                    type: 'asset'
-                }
-            ]
-        },
-        devtool: 'cheap-module-source-map',
-        resolve: {
-            alias: {
-                'handlebars/runtime': 'handlebars/dist/cjs/handlebars.runtime',
-                'handlebars': 'handlebars/dist/cjs/handlebars'
-            }
-        },
-        optimization: {
-          minimizer: [
-            '...',
-            new CssMinimizerPlugin()
+  return {
+    entry: {
+      background: './src/background/background.js',
+      popup: './src/popup/popup.js',
+      content: './src/content/index.js'
+    },
+    output: {
+      path: path.resolve(devPath),
+      filename: '[name]/[name].js',
+      clean: true
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.(css)$/i,
+          use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader'
           ]
+        },
+        {
+          test: /\.(png|svg)$/,
+          type: 'asset'
         }
-    };
+      ]
+    },
+    devtool: 'cheap-module-source-map',
+    resolve: {
+      alias: {
+        'handlebars/runtime': 'handlebars/dist/cjs/handlebars.runtime',
+        'handlebars': 'handlebars/dist/cjs/handlebars'
+      }
+    },
+    optimization: {
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin()
+      ]
+    }
+  }
 }
 
 export default async function (env) {
