8d66fa6b8adb0b637d1100ae5d357bc63d6d030f
Ionut Colceriu <contact@ghinda.net>
2022-03-22 16:42:02 +0200

    Fix running parseTemplate tests.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 import '../src/content/helpers/moment.spec.js'
 import '../src/content/search.spec.js'
 import '../src/content/utils/html-to-text.spec.js'
-// TODO doesn't work because of importing store
-// import '../src/content/utils/parse-template.spec.js'
+import '../src/content/utils/parse-template.spec.js'
 import '../src/content/editors/editor-prosemirror.spec.js'
