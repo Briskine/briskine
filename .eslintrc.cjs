6041d88d74bcfaf3b7efddc0459cfff89507a1e6
Ionut Colceriu <contact@ghinda.net>
2022-05-06 12:21:42 +0300

    Switch to eslint for linting.

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  ignorePatterns: [
+    'ext/',
+    'test/bundle/',
+  ],
+}
