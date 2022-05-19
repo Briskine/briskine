46d23e3ea2596bb3f3e3d3a7f845e7e8c6d9a7ae
Ionut Colceriu <contact@ghinda.net>
2022-05-19 13:09:55 +0300

    Use substring instead of subsrt.

--- src/content/autocomplete.js
+++ src/content/autocomplete.js
@@ -66,7 +66,7 @@ export function getSelectedWord (params) {
       beforeSelection.lastIndexOf('\n'),
       beforeSelection.lastIndexOf('<br>'),
     )
-  const text = beforeSelection.substr(start)
+  const text = beforeSelection.substring(start)
   const end = start + text.length
 
   return {
