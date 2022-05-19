b51f2d3b004947084a7c50fc070803929574a863
Ionut Colceriu <contact@ghinda.net>
2022-05-19 13:02:37 +0300

    Clean up the editor textarea module.

--- src/content/editors/editor-textarea.js
+++ src/content/editors/editor-textarea.js
@@ -1,39 +1,39 @@
 /* Generic editors
  */
 
-import htmlToText from '../utils/html-to-text.js';
+import htmlToText from '../utils/html-to-text.js'
 
 export function insertTextareaTemplate (params = {}) {
   // restore focus to the editable area
-  params.element.focus();
+  params.element.focus()
 
   // convert html to text for textfields
-  const content = htmlToText(params.text);
-  const textfieldValue = params.element.value;
-  let cursorOffset = params.word.end + content.length;
+  const content = htmlToText(params.text)
+  const textfieldValue = params.element.value
+  let cursorOffset = params.word.end + content.length
 
   // if the current word matches the shortcut then remove it
   // otherwise skip it (ex: from dialog)
-  let wordStart = params.word.start;
+  let wordStart = params.word.start
   if (params.word.text === params.quicktext.shortcut) {
-      // decrease the cursor offset with the removed text length
-      cursorOffset = cursorOffset - params.word.text.length;
+    // decrease the cursor offset with the removed text length
+    cursorOffset = cursorOffset - params.word.text.length
   } else {
-      // don't delete anything in the textarea
-      // just add the qt
-      wordStart = params.word.end;
+    // don't delete anything in the textarea
+    // just add the qt
+    wordStart = params.word.end
   }
 
-  params.element.value = textfieldValue.substr(0, wordStart) + content + textfieldValue.substr(params.word.end);
+  params.element.value = textfieldValue.substr(0, wordStart) + content + textfieldValue.substr(params.word.end)
 
   // set focus at the end of the added template
-  params.element.setSelectionRange(cursorOffset, cursorOffset);
+  params.element.setSelectionRange(cursorOffset, cursorOffset)
 
   // trigger multiple change events,
   // for frameworks and scripts to notice changes to the editable fields.
-  [ 'input', 'change' ].forEach((eventType) => {
-      params.element.dispatchEvent(new Event(eventType, {bubbles: true}));
-  });
+  Array('input', 'change').forEach((eventType) => {
+      params.element.dispatchEvent(new Event(eventType, {bubbles: true}))
+  })
 
-  return;
+  return
 }
