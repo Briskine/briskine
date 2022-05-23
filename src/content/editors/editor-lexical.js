c922805923e4851cd1533f29d56ece36ffd08430
Ionut Colceriu <contact@ghinda.net>
2022-05-23 11:40:32 +0300

    Improve support for lexical and fix keyboard completion. Add support for html formatting in lexical.

--- src/content/editors/editor-lexical.js
+++ src/content/editors/editor-lexical.js
@@ -2,30 +2,36 @@
  * https://lexical.dev/
  */
 
-import htmlToText from '../utils/html-to-text.js';
-
 export function isLexical (element) {
   return element.dataset.lexicalEditor
 }
 
-// TODO keyboard completion stopped working in the lexical playground
-// templates are removed after being inserted
-// TODO dialog insert still works, but templates are one-liners
 export function insertLexicalTemplate (params = {}) {
   const selection = window.getSelection()
   const range = selection.getRangeAt(0)
   const focusNode = selection.focusNode
+
+  let removeShortcut = Promise.resolve()
 
   // delete shortcut
   if (params.word.text === params.quicktext.shortcut) {
     range.setStart(focusNode, params.word.start)
     range.setEnd(focusNode, params.word.end)
     range.deleteContents()
+    params.element.dispatchEvent(new Event('input', {bubbles: true}))
+
+    // lexical needs a second to update the editor state
+    removeShortcut = new Promise((resolve) => setTimeout(resolve, 100))
   }
 
-  // only supports plain text
-  const content = htmlToText(params.text)
-
-  // needs an extra newline at the start
-  return document.execCommand('insertText', false, `\n${content}`)
+  removeShortcut.then(() => {
+    const clipboardData = new DataTransfer()
+    clipboardData.setData('text/html', params.text)
+    const customPasteEvent = new ClipboardEvent('paste', {
+        clipboardData: clipboardData,
+        bubbles: true,
+    })
+    params.element.dispatchEvent(customPasteEvent)
+    clipboardData.clearData()
+  })
 }
