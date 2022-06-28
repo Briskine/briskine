e43d1157caf2d16834e2f1419e9cb700dd47ba2f
Ionut Colceriu <contact@ghinda.net>
2022-06-28 17:36:32 +0300

    Fix lint errors in cypress setup.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
         const extensionFolder = path.resolve('./ext')
 
--- cypress/e2e/textarea/textarea.cy.js
+++ cypress/e2e/textarea/textarea.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+/* globals cy, describe, before, it */
 
 describe('Textarea', () => {
   before(() => {
