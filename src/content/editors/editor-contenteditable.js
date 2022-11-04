8587f06affc37fb27c51080ff7a17b1774189f5e
Ionuț Colceriu <contact@ghinda.net>
2022-11-04 10:18:47 +0200

    Fix Safari caret position when inserting with keyboard shortcut (#484)
    * Fix the caret position on Safari, to be placed at the end of the inserted template, when inserting templates with the keyboard shortcut.
    * When using `range.insertNode()` on the existing range on Safari, the new node's contents are not selected, causing `range.collapse()` to place the caret at the start of the inserted node - rather than the end.
    * As a workaround, clone the main range before making any operations on it, and add the modified range to the selection.

--- src/content/editors/editor-contenteditable.js
+++ src/content/editors/editor-contenteditable.js
@@ -7,7 +7,13 @@ export function isContentEditable (element) {
 
 export function insertContentEditableTemplate (params = {}) {
   const selection = window.getSelection()
-  const range = selection.getRangeAt(0)
+  // run operations on a cloned range, rather than the original range,
+  // to fix issues with not correctly positioning the cursor on Safari.
+  // using range.insertNode() doesn't select the inserted contents, *only on Safari*.
+  // the range object will mistakenly behave as if the contents are selected,
+  // while the selection object will reflect the real state.
+  // using a cloned range fixes the issue.
+  const range = selection.getRangeAt(0).cloneRange()
   const focusNode = selection.focusNode
 
   // delete shortcut
@@ -20,6 +26,9 @@ export function insertContentEditableTemplate (params = {}) {
   const templateNode = range.createContextualFragment(params.text)
   range.insertNode(templateNode)
   range.collapse()
+
+  selection.removeAllRanges()
+  selection.addRange(range)
 
   // trigger multiple change events,
   // for frameworks and scripts to notice changes to the editable fields.
