9a5a754e0d617fda02159a6f46fddac7c67e4dc0
Ionut Colceriu <contact@ghinda.net>
2022-12-06 15:48:16 +0200

    Fix issues with having an extra newline when inserting templates from the dialog, with no preceding text. When inserting templates from the dialog, always replace selected text with the template.

--- src/content/editors/editor-contenteditable.js
+++ src/content/editors/editor-contenteditable.js
@@ -15,16 +15,39 @@ export function insertContentEditableTemplate (params = {}) {
   // the range object will mistakenly behave as if the contents are selected,
   // while the selection object will reflect the real state.
   // using a cloned range fixes the issue.
-  const range = selection.getRangeAt(0).cloneRange()
+  let range = selection.getRangeAt(0).cloneRange()
 
-  // delete shortcut
   if (params.word.text === params.quicktext.shortcut) {
+    // delete matched shortcut
     range.setStart(selection.focusNode, params.word.start)
     range.setEnd(selection.focusNode, params.word.end)
-  }
+    range.deleteContents()
+  } else {
+    // delete previous selection, if no shortcut match.
+    // we're most probably inserting from the dialog.
+    range.deleteContents()
 
-  // always delete the selected text
-  range.deleteContents()
+    // empty paragraphs usually contain a BR tag in contenteditable,
+    // which is replaced by text nodes if we type anything inside the line.
+    // if we insert text on a blank line, containing a BR (eg. when inserting from the dialog),
+    // we'll get an extra newline after the template.
+    // even if technically correct, this can look unexpected and we'd prefer the empty line
+    // to be replaced with the template (instead of the template being prepended before it).
+
+    // detect if we're on an empty line containing only whitespace
+    if (
+      selection.anchorNode.nodeType === document.ELEMENT_NODE &&
+      selection.focusNode === selection.anchorNode &&
+      selection.focusNode.innerText.trim() === ''
+    ) {
+      // select all nodes on the line
+      selection.setBaseAndExtent(selection.anchorNode, 0, selection.focusNode, selection.focusNode.childNodes.length)
+      // get an updated range reference
+      range = selection.getRangeAt(0).cloneRange()
+      // delete all child nodes
+      range.deleteContents()
+    }
+  }
 
   const templateNode = range.createContextualFragment(params.text)
   range.insertNode(templateNode)
