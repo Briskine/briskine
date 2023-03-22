e696e4a160dd09e7d800f1c9292e69a81a16002c
Ionut Colceriu <contact@ghinda.net>
2023-03-22 16:51:33 +0200

    Tweak the swiperight functionality.

--- src/content/swipe.js
+++ src/content/swipe.js
@@ -6,36 +6,21 @@ const threshold = 20
 const timeout = 500
 
 let done = () => {}
-let xdown = null
-let xup = null
+let xstart = null
 let touchTime = null
-let startElement = null
 
 function touchstart (e) {
-  startElement = e.target
   touchTime = Date.now()
-  xdown = e.touches[0].clientX
-}
-
-function touchmove (e) {
-  if (!xdown) {
-    return
-  }
-
-  xup = e.touches[0].clientX
+  xstart = e.touches[0].clientX
 }
 
 function touchend (e) {
-  if (startElement !== e.target) {
-    return
-  }
-
-  const xdiff = xdown - xup
+  const xend = e.changedTouches[0].clientX
+  const xdiff = xend - xstart
   const timeDiff = Date.now() - touchTime
   if (
-    Math.abs(xdiff) > threshold
+    xdiff > threshold
     && timeDiff < timeout
-    && xdiff < 0
   ) {
     done(e)
   }
@@ -45,12 +30,10 @@ export function swipebind (callback = () => {}) {
   done = callback
 
   window.addEventListener('touchstart', touchstart, true)
-  window.addEventListener('touchmove', touchmove, true)
   window.addEventListener('touchend', touchend, true)
 }
 
 export function swipeunbind () {
   window.removeEventListener('touchstart', touchstart, true)
-  window.removeEventListener('touchmove', touchmove, true)
   window.removeEventListener('touchend', touchend, true)
 }
