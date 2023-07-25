603a2e4213f8bbda21bb95ed52323c0a07e85b79
Ionut Colceriu <contact@ghinda.net>
2023-07-25 12:40:07 +0300

    Clean up the moment handlebars helper.

--- src/content/helpers/moment.js
+++ src/content/helpers/moment.js
@@ -1,71 +1,69 @@
-import Handlebars from 'handlebars';
-import moment from 'moment';
+import Handlebars from 'handlebars'
+import moment from 'moment'
 
-function momentHelper(str, options) {
-    // check if str is a valid date
-    let dateString;
-    if (typeof str === 'string' && moment(str).isValid()) {
-        dateString = str;
-    }
-    const date = moment(dateString);
+function momentHelper (dateParam, options) {
+  // check if str is a valid date
+  let dateString
+  if (typeof dateParam === 'string' && moment(dateParam).isValid()) {
+    dateString = dateParam
+  }
+  const date = moment(dateString)
 
-    let opts = {};
-    if (typeof str === 'object') {
-        opts = str;
-    } else if (typeof options === 'object') {
-        opts = options;
-    }
+  let opts = {}
+  if (typeof dateParam === 'object') {
+    opts = dateParam
+  } else if (typeof options === 'object') {
+    opts = options
+  }
 
-    // get the default locale from the browser
-    const defaultLocale = navigator.language || 'en'
-    opts = Object.assign(
-        {
-            locale: defaultLocale,
-            format: 'MMMM DD YYYY'
-        },
-        opts.hash
-    );
+  // get the default locale from the browser
+  const defaultLocale = navigator.language || 'en'
+  opts = Object.assign(
+    {
+      locale: defaultLocale,
+      format: 'MMMM DD YYYY'
+    },
+    opts.hash,
+  )
 
-    date.locale(opts.locale);
+  date.locale(opts.locale)
 
-    let display = 'format';
-    let displayParams = [];
-    const displayMethods = [
-        'format',
-        'fromNow',
-        'toNow',
-        'daysInMonth',
-        'week',
-        'weeks',
-    ];
+  let display = 'format'
+  let displayParams = []
+  const displayMethods = [
+    'format',
+    'fromNow',
+    'toNow',
+    'daysInMonth',
+    'week',
+    'weeks',
+  ]
 
-    for (const key in opts) {
-        // handle only last display method
-        if (displayMethods.includes(key)) {
-            display = key;
-            if (opts[key] !== '') {
-              displayParams = [opts[key]]
-            } else {
-              displayParams = []
-            }
-            continue;
-        }
-
-        // only supported methods
-        if (
-            typeof date[key] === 'function' &&
-            !displayMethods.includes(key)
-        ) {
-            // support multiple function params with ;
-            const params = typeof opts[key] === 'string' ?
-                opts[key].split(';').map((s) => s.trim()) :
-                [opts[key]];
-
-            date[key].apply(date, params);
-        }
+  for (const key in opts) {
+    // handle only last display method
+    if (displayMethods.includes(key)) {
+      display = key
+      if (opts[key] !== '') {
+        displayParams = [opts[key]]
+      } else {
+        displayParams = []
+      }
+      continue;
     }
 
-    return date[display].apply(date, displayParams);
+    // only supported methods
+    if (
+      typeof date[key] === 'function' &&
+      !displayMethods.includes(key)
+    ) {
+      // support multiple function params with ;
+      const params = typeof opts[key] === 'string' ? opts[key].split(';').map((s) => s.trim()) : [opts[key]]
+
+      date[key].apply(date, params)
+    }
+  }
+
+  return date[display].apply(date, displayParams)
 }
 
-Handlebars.registerHelper('moment', momentHelper);
+Handlebars.registerHelper('moment', momentHelper)
