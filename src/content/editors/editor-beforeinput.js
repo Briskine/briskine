541309e1fcc94152a49907aff0c05235321d69c2
Ionut Colceriu <contact@ghinda.net>
2023-08-21 12:22:16 +0300

    Tweak formatting in the beforeinput editor plugin.

--- src/content/editors/editor-beforeinput.js
+++ src/content/editors/editor-beforeinput.js
@@ -15,33 +15,33 @@ export function isBeforeInputEditor (element) {
 }
 
 export function insertBeforeInputTemplate (params = {}) {
-    params.element.focus();
+  params.element.focus()
 
-    // Slate won't handle the text manipulation events until it notices we restored focus.
-    // The timeout is required for inserting templates with the dialog.
-    setTimeout(() => {
-        // Slate uses onbeforeinput exclusively, and does not notice content inserted from outside.
-        // https://docs.slatejs.org/concepts/xx-migrating#beforeinput
-        // Use the beforeinput-specific method to insert and remove text,
-        // using custom synthetic beforeinput events.
-        // Slate and Lexical handle beforeinput events with stadard inputType's
-        // https://github.com/ianstormtaylor/slate/blob/16ff44d0566889a843a346215d3fb7621fc0ed8c/packages/slate-react/src/components/editable.tsx#L193
-        if (params.word.text === params.quicktext.shortcut) {
-            const deleteWordBackward = new InputEvent('beforeinput', {
-                bubbles: true,
-                // template shortcut will always be the word before the cursor
-                inputType: 'deleteWordBackward'
-            });
-            params.element.dispatchEvent(deleteWordBackward);
-        }
+  // Slate won't handle the text manipulation events until it notices we restored focus.
+  // The timeout is required for inserting templates with the dialog.
+  setTimeout(() => {
+    // Slate uses onbeforeinput exclusively, and does not notice content inserted from outside.
+    // https://docs.slatejs.org/concepts/xx-migrating#beforeinput
+    // Use the beforeinput-specific method to insert and remove text,
+    // using custom synthetic beforeinput events.
+    // Slate and Lexical handle beforeinput events with stadard inputType's
+    // https://github.com/ianstormtaylor/slate/blob/16ff44d0566889a843a346215d3fb7621fc0ed8c/packages/slate-react/src/components/editable.tsx#L193
+    if (params.word.text === params.quicktext.shortcut) {
+      const deleteWordBackward = new InputEvent('beforeinput', {
+        bubbles: true,
+        // template shortcut will always be the word before the cursor
+        inputType: 'deleteWordBackward'
+      })
+      params.element.dispatchEvent(deleteWordBackward)
+    }
 
-        // only supports plain text
-        const content = htmlToText(params.text);
-        const insertText = new InputEvent('beforeinput', {
-            bubbles: true,
-            inputType: 'insertText',
-            data: content
-        });
-        params.element.dispatchEvent(insertText);
-    });
+    // only supports plain text
+    const content = htmlToText(params.text)
+    const insertText = new InputEvent('beforeinput', {
+      bubbles: true,
+      inputType: 'insertText',
+      data: content
+    })
+    params.element.dispatchEvent(insertText)
+  })
 }
