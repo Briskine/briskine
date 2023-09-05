5275f81ff97221ffc46a032a92a8e012a53113e9
Ionut Colceriu <contact@ghinda.net>
2023-09-05 11:39:06 +0300

    Add a mocha test replicating the issue with prosemirror removing anchors, when they are the only content of the template.

--- src/content/editors/editor-prosemirror.spec.js
+++ src/content/editors/editor-prosemirror.spec.js
@@ -1,7 +1,7 @@
 /* globals describe, it */
 import {expect} from 'chai';
 
-import {parseProseMirrorContent} from './editor-prosemirror.js';
+import {parseProseMirrorContent, insertProseMirrorTemplate} from './editor-prosemirror.js';
 
 describe('editor ProseMirror', () => {
   it('should add brs after block nodes', () => {
@@ -33,6 +33,73 @@ describe('editor ProseMirror', () => {
       <div>one</div>
       <div>two</div>
     `)).to.equal('<div>one</div><br><div>two</div>')
+  })
+
+  it.only('should insert template containing only anchor', function (done) {
+    this.timeout(20000)
+
+    const $link = document.createElement('link')
+    $link.rel = 'stylesheet'
+    $link.href = 'https://prosemirror.net/css/editor.css'
+    document.head.appendChild($link)
+
+    const $script = document.createElement('script')
+    $script.type = 'module'
+    $script.textContent = `
+      import {EditorState} from 'https://cdn.jsdelivr.net/npm/prosemirror-state@1/+esm'
+      import {EditorView} from 'https://cdn.jsdelivr.net/npm/prosemirror-view@1/+esm'
+      import {Schema, DOMParser} from 'https://cdn.jsdelivr.net/npm/prosemirror-model@1/+esm'
+      import {schema} from 'https://cdn.jsdelivr.net/npm/prosemirror-schema-basic@1/+esm'
+      import {addListNodes} from 'https://cdn.jsdelivr.net/npm/prosemirror-schema-list@1/+esm'
+      import {exampleSetup} from 'https://cdn.jsdelivr.net/npm/prosemirror-example-setup@1/+esm'
+
+      const $editor = document.createElement('div')
+      const $content = document.createElement('div')
+      document.body.appendChild($editor)
+
+      // Mix the nodes from prosemirror-schema-list into the basic schema to
+      // create a schema with list support.
+      const mySchema = new Schema({
+        nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
+        marks: schema.spec.marks
+      })
+
+      new EditorView($editor, {
+        state: EditorState.create({
+          doc: DOMParser.fromSchema(mySchema).parse($content),
+          plugins: exampleSetup({schema: mySchema})
+        })
+      })
+
+      window.dispatchEvent(new Event('prosemirror-ready'))
+    `
+
+    window.addEventListener('prosemirror-ready', () => {
+      const template = '<div><a href="https://www.briskine.com">briskine.com</a></div>'
+      const $editor = document.querySelector('[contenteditable]')
+
+      $editor.focus()
+      insertProseMirrorTemplate({
+        element: $editor,
+        text: template,
+        word: {
+          start: 0,
+          end: 0,
+          text: '',
+        },
+        quicktext: {},
+      })
+
+      // give it a second to parse the template
+      setTimeout(() => {
+        // console.log($editor.innerHTML)
+        // expect($editor.innerHTML).to.equal('<p><a href="https://www.briskine.com">briskine.com</a></p>')
+        expect($editor.innerHTML).to.equal('<p>briskine.com</p>')
+        done()
+      })
+    }, {once: true})
+
+    document.body.appendChild($script)
   })
 })
 
