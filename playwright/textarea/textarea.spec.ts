4af740a04a5671cee4540e7337a5b17ebbc80fc8
Ionut Colceriu <contact@ghinda.net>
2024-03-18 16:58:03 +0200

    Fix race condition with destroying the content script before init was run.

--- playwright/textarea/textarea.spec.ts
+++ playwright/textarea/textarea.spec.ts
@@ -1,18 +1,34 @@
 import { test, expect } from '../fixtures.ts'
 
-test.beforeEach(async ({page}) => {
-  await page.goto('/textarea/textarea.html')
-})
+test.describe('Textarea', () => {
+  test.beforeEach(async ({page}) => {
+    await page.goto('/textarea/textarea.html')
+  })
 
-test('should insert template with keyboard shortcut in textarea', async ({page}) => {
-  await page.getByTestId('textarea').fill('kr')
-  // TODO BUG triggering keydown/up immediately, with no delay, causes the template to be inserted twice
-  // probably caused by mousetrap attaching events to both
-  // in the real-world, we'll always have at least a 10ms delay, so probably not a real issue
-  await page.getByTestId('textarea').press('Tab', {delay: 10})
-  await expect(page.getByTestId('textarea')).toHaveValue('Kind regards,\n.')
+  test.afterEach(async ({page}) => {
+    await page.getByTestId('textarea').fill('')
+  })
 
-  // cy.get('textarea').type('kr').tab()
-  // cy.get('textarea').should('have.value', 'Kind regards,\n.')
-  // cy.get('textarea').type('{selectAll}{del}')
+  test('should insert template with keyboard shortcut in textarea', async ({page}) => {
+    await page.getByTestId('textarea').fill('kr')
+    await page.getByTestId('textarea').press('Tab')
+    await expect(page.getByTestId('textarea')).toHaveValue('Kind regards,\n.')
+  })
+
+  test('should insert template from dialog in textarea', async ({page}) => {
+    await page.getByTestId('textarea').press('Control+ ')
+    await expect(page.getByPlaceholder('Search templates...')).toBeVisible()
+
+    // cy.get('textarea').type('{ctrl} ')
+    // cy.get('[visible=true]').should('be.visible')
+    // cy.focused()
+    //   .shadow()
+    //   .find('input[type=search]')
+    //   .type('nic', {force: true})
+    //   .wait(500)
+    //   .type('{enter}')
+    //
+    // cy.get('textarea').should('have.value', 'It was nice talking to you.')
+  })
+
 })
