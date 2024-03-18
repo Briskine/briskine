347765f57aae5354bca4c8acd22e4f27e42b5cca
Ionut Colceriu <contact@ghinda.net>
2024-03-18 17:18:07 +0200

    Port the cypress textarea tests to playwright.

--- playwright/textarea/textarea.spec.ts
+++ playwright/textarea/textarea.spec.ts
@@ -10,25 +10,40 @@ test.describe('Textarea', () => {
   })
 
   test('should insert template with keyboard shortcut in textarea', async ({page}) => {
-    await page.getByTestId('textarea').fill('kr')
-    await page.getByTestId('textarea').press('Tab')
-    await expect(page.getByTestId('textarea')).toHaveValue('Kind regards,\n.')
+    const textarea = await page.getByTestId('textarea')
+    await textarea.fill('kr')
+    await textarea.press('Tab')
+    await expect(textarea).toHaveValue('Kind regards,\n.')
   })
 
   test('should insert template from dialog in textarea', async ({page}) => {
-    await page.getByTestId('textarea').press('Control+ ')
-    await expect(page.getByPlaceholder('Search templates...')).toBeVisible()
-
-    // cy.get('textarea').type('{ctrl} ')
-    // cy.get('[visible=true]').should('be.visible')
-    // cy.focused()
-    //   .shadow()
-    //   .find('input[type=search]')
-    //   .type('nic', {force: true})
-    //   .wait(500)
-    //   .type('{enter}')
-    //
-    // cy.get('textarea').should('have.value', 'It was nice talking to you.')
+    const textarea = await page.getByTestId('textarea')
+    await textarea.press('Control+ ')
+    const search = await page.getByPlaceholder('Search templates...')
+    await expect(search).toBeVisible()
+    await search.fill('nic')
+    await page.waitForTimeout(500)
+    await search.press('Enter')
+    await expect(textarea).toHaveValue('It was nice talking to you.')
   })
+
+  test('should insert template with keyboard shortcut in input', async ({page}) => {
+    const input = await page.getByTestId('input')
+    await input.fill('kr')
+    await input.press('Tab')
+    await expect(input).toHaveValue('Kind regards,.')
+  })
+
+  test('should insert template from dialog in input', async ({page}) => {
+    const input = await page.getByTestId('input')
+    await input.press('Control+ ')
+    const search = await page.getByPlaceholder('Search templates...')
+    await search.fill('nic')
+    await page.waitForTimeout(500)
+    await search.press('Enter')
+    await expect(input).toHaveValue('It was nice talking to you.')
+  })
+
+
 
 })
