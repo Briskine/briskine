b4f5f05ee0a2a080b6794c3063e082e5748465be
Ionut Colceriu <contact@ghinda.net>
2024-03-20 13:00:03 +0200

    Tweak playwright config.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,42 +1,17 @@
-import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+import { defineConfig, devices } from '@playwright/test'
 
 const baseURL = 'http://127.0.0.1:8080'
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './playwright',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  forbidOnly: false,
+  retries: 0,
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: 'line',
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: baseURL,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  // webServer: {
-  //   // command: 'npm run start',
-  //   url: 'http://127.0.0.1:3333',
-  // },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -46,32 +21,10 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npx http-server ./playwright',
     url: baseURL,
     reuseExistingServer: true,
   },
-});
+})
