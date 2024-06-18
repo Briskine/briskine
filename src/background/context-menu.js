759d022c6e2e79affe9453b957e70f572e12b4da
Ionut Colceriu <contact@ghinda.net>
2024-06-18 16:04:50 +0300

    Initial work on the new context menus.

--- src/background/context-menu.js
+++ src/background/context-menu.js
@@ -1,32 +1,56 @@
+/* globals MANIFEST */
 import browser from 'webextension-polyfill'
 
-/* globals MANIFEST */
 import config from '../config.js'
+import {openPopup} from '../store/open-popup.js'
 
-const saveAsTemplate = 'saveAsTemplate'
+const saveAsTemplateMenu = 'saveAsTemplate'
+const openDialogMenu = 'openDialog'
+const signInMenu = 'signIn'
 
-function getSelection () {
+function getSelectedText () {
   return window.getSelection()?.toString?.()
 }
 
-async function clickContextMenu (info) {
-  if (info.menuItemId === saveAsTemplate) {
-    let body = info.selectionText
-    if (MANIFEST === '3') {
-      const activeTab = await browser.tabs.query({active: true})
-      const selection = await browser.scripting.executeScript({
-        target: {
-          tabId: activeTab[0].id,
-        },
-        func: getSelection,
-      })
-      // replace newlines with brs
-      body = selection[0]?.result?.replace?.(/(?:\r\n|\r|\n)/g, '<br>')
-    }
-
-    browser.tabs.create({
-      url: `${config.functionsUrl}/template/new?body=${encodeURIComponent(body)}`
+async function saveAsTemplateAction (info) {
+  let body = info.selectionText
+  if (MANIFEST === '3') {
+    const activeTab = await browser.tabs.query({active: true})
+    const selection = await browser.scripting.executeScript({
+      target: {
+        tabId: activeTab[0].id,
+      },
+      func: getSelectedText,
     })
+    // replace newlines with brs
+    if (selection[0].result) {
+      body = selection[0].result.replace(/(?:\r\n|\r|\n)/g, '<br>')
+    }
+  }
+
+  browser.tabs.create({
+    url: `${config.functionsUrl}/template/new?body=${encodeURIComponent(body)}`
+  })
+}
+
+async function openDialogAction (info) {
+}
+
+async function signInAction (info) {
+  return openPopup()
+}
+
+async function clickContextMenu (info) {
+  if (info.menuItemId === saveAsTemplateMenu) {
+    return saveAsTemplateAction(info)
+  }
+
+  if (info.menuItemId === openDialogMenu) {
+    return openDialogAction(info)
+  }
+
+  if (info.menuItemId === signInMenu) {
+    return signInAction(info)
   }
 }
 
@@ -36,10 +60,41 @@ async function setupContextMenus () {
   }
 
   await browser.contextMenus.removeAll()
+
+  const parentMenu = 'briskineMenu'
   browser.contextMenus.create({
-    title: 'Save "%s" as a template',
+    contexts: ['all'],
+    title: 'Briskine',
+    id: parentMenu,
+  })
+
+  // TODO show when no logged in
+  browser.contextMenus.create({
+    contexts: ['all'],
+    title: 'Sign in to access your templates',
+    id: signInMenu,
+    parentId: parentMenu,
+  })
+
+  browser.contextMenus.create({
+    contexts: ['all'],
+    type: 'separator',
+    parentId: parentMenu,
+    id: 'mainSeparator',
+  })
+
+  browser.contextMenus.create({
     contexts: ['selection'],
-    id: saveAsTemplate,
+    title: 'Save "%s" as a template',
+    parentId: parentMenu,
+    id: saveAsTemplateMenu,
+  })
+
+  browser.contextMenus.create({
+    contexts: ['all'],
+    title: 'Open Briskine dialog',
+    parentId: parentMenu,
+    id: openDialogMenu,
   })
 
   browser.contextMenus.onClicked.addListener(clickContextMenu)
