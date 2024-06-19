681fc29c69c2ba2229ae69ded171521655378ce6
Ionut Colceriu <contact@ghinda.net>
2024-06-19 13:04:10 +0300

    Use the scripting api namespace in manifest v2.

--- src/background/update-content.js
+++ src/background/update-content.js
@@ -1,4 +1,3 @@
-/* globals MANIFEST */
 import browser from 'webextension-polyfill'
 
 browser.runtime.onInstalled.addListener(async (details) => {
@@ -14,34 +13,22 @@ browser.runtime.onInstalled.addListener(async (details) => {
   const tabs = await browser.tabs.query({url: contentScripts.matches})
 
   for (const tab of tabs) {
-    if (MANIFEST === '2') {
-      // manifest v2
-      for (const file of styles) {
-        await browser.tabs.removeCSS(tab.id, {file: file})
-        browser.tabs.insertCSS(tab.id, {file: file})
-      }
-
-      scripts.forEach((script) => {
-        browser.tabs.executeScript(tab.id, {file: script})
-      })
-    } else {
-      // manifest v3
-      const cssInjectParams = {
-        target: {
-          tabId: tab.id,
-        },
-        files: styles,
-      }
-
-      await browser.scripting.removeCSS(cssInjectParams)
-      browser.scripting.insertCSS(cssInjectParams)
-
-      browser.scripting.executeScript({
-        target: {
-          tabId: tab.id,
-        },
-        files: scripts
-      })
+    const cssInjectParams = {
+      target: {
+        tabId: tab.id,
+        allFrames: true,
+      },
+      files: styles,
     }
+
+    await browser.scripting.removeCSS(cssInjectParams)
+    browser.scripting.insertCSS(cssInjectParams)
+
+    browser.scripting.executeScript({
+      target: {
+        tabId: tab.id,
+      },
+      files: scripts
+    })
   }
 })
