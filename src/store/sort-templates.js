f20b30ad882e5fc23cc77c2fb4af5a0cc7568050
Ionut Colceriu <contact@ghinda.net>
2024-07-26 16:39:08 +0300

    Don't use toSorted, to support Chrome 109 - last version available on Win7.

--- src/store/sort-templates.js
+++ src/store/sort-templates.js
@@ -1,21 +1,24 @@
-export default function sortTemplates (templates = [], sort = 'last_used', lastUsed = {}) {
+export default function sortTemplates (list = [], sort = 'last_used', lastUsed = {}) {
+  // Chrome 109 (last version supported on Windows 7) don't support toSorted,
+  // so we clone the array and use regular sort.
+  const templates = structuredClone(list)
   if (['title', 'shortcut'].includes(sort)) {
     return templates
-      .toSorted((a, b) => {
+      .sort((a, b) => {
         return a[sort].localeCompare(b[sort])
       })
   }
 
   if (sort === 'modified_datetime') {
     return templates
-      .toSorted((a, b) => {
+      .sort((a, b) => {
         return new Date(b.modified_datetime || 0) - new Date(a.modified_datetime || 0)
       })
   }
 
   // default last_used sort
   return templates
-    .toSorted((a, b) => {
+    .sort((a, b) => {
       return new Date(lastUsed[b.id] || 0) - new Date(lastUsed[a.id] || 0)
     })
 }
