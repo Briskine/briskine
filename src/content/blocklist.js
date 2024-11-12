30fc34eb138afd4eb29a828ccf4a8e1ab3b93ded
Ionut Colceriu <contact@ghinda.net>
2024-11-12 16:04:06 +0200

    Fix issues with the slack pdf viewer. The pdf viewer stops working when the dom is modified by extensions. Blocklist briskine for the pdf viewer.

--- src/content/blocklist.js
+++ src/content/blocklist.js
@@ -1,5 +1,10 @@
 const blocklistPrivate = [
   '.briskine.com',
+
+  // slack pdf viewer stops working (considering the pdf corrupt),
+  // when the iframe dom is modified.
+  // the pdf viewer is used for previewing multiple attachment types (pdf, docx, etc.).
+  '//app.slack.com/pdf-viewer?',
 ]
 
 function getBlocklist (settings = {}) {
