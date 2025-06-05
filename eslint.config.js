8f94b2d1bec9d4703cefe818188582ef21f6b9a7
Ionut Colceriu <contact@ghinda.net>
2025-06-05 17:11:31 +0300

    Add stylistic eslint plugin.

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,18 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import solid from 'eslint-plugin-solid/configs/recommended'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['warn', 'never'],
+      '@stylistic/quotes': ['warn', 'single'],
+    }
+  },
   js.configs.recommended,
   solid,
   {
