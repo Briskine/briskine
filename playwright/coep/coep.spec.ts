a8d55b649bee33e7d4e5e97fc2f909ab7078506a
Ionuț Colceriu <contact@ghinda.net>
2025-10-17 10:56:49 +0300

    Re-enable use_dynamic_url (#579)
    * Re-enable `use_dynamic_url` after adding `credentialless` workaround
    for iframes on sites with COEP.
    * Add a playwright test for sites with COEP.

---
+++ playwright/coep/coep.spec.ts
@@ -0,0 +1,35 @@
+import {test, expect} from '../fixtures.ts'
+
+test.describe('Cross-Origin-Embedder-Policy=require-corp', () => {
+  test.beforeEach(async ({page}) => {
+    const coepHeaders = {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    }
+
+    await page.route('/coep/coep.html', async (route) => {
+      const response = await page.request.fetch(route.request())
+      const body = await response.body()
+      await route.fulfill({
+        status: response.status(),
+        headers: {
+          ...response.headers(),
+          ...coepHeaders
+        },
+        body: body
+      })
+    })
+
+    await page.goto('/coep/coep.html')
+  })
+
+  test.afterEach(async ({page}) => {
+    await page.getByRole('textbox').fill('')
+  })
+
+  test('should insert template with keyboard shortcut', async ({page}) => {
+    const textbox = page.getByRole('textbox')
+    await textbox.fill('kr')
+    await textbox.press('Tab')
+    await expect(textbox).toHaveText('Kind regards,.')
+  })
+})
