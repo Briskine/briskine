b6073a348a391e999fb831ceeb61b28dfece87ca
colceriu-ovidiu <colceriu.ovidiu@gmail.com>
2025-11-27 17:30:09 +0200

    add listener on iframe

--- playwright/util/setup-listen-message.js
+++ playwright/util/setup-listen-message.js
@@ -1,6 +1,23 @@
-window.addEventListener('message', (e) => {
+
+let messageListener = (e) => {
     if (e?.data === 'briskine-ready') {
         // eslint-disable-next-line no-console
         console.log('BSKN inited')
     }
-})
+}
+
+window.addEventListener('message', messageListener)
+
+let setupListenMessage = () => {
+    document.querySelectorAll("iframe").forEach((iframeElem) =>
+        iframeElem.contentWindow.addEventListener('message', messageListener)
+    )
+}
+
+document.onreadystatechange = function()
+{
+    if (document.readyState === 'complete')
+    {
+        setupListenMessage()
+    }
+};
