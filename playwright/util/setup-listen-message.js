a30031ed04529eb40e2277ad8335e4c379934a1f
Ionut Colceriu <contact@ghinda.net>
2025-12-15 13:24:26 +0200

    Formatting for the new extension ready script.

--- playwright/util/setup-listen-message.js
+++ playwright/util/setup-listen-message.js
@@ -1,39 +1,39 @@
-
 const promiseMainWindow = new Promise((resolve) => {
-    window.addEventListener('message', (e) => { 
-        if (e?.data === 'briskine-ready') {
-            resolve(true)
-        }
-    })
+  window.addEventListener('message', (e) => {
+    if (e?.data === 'briskine-ready') {
+      resolve()
+    }
+  })
 })
 
 const promiseDocumentReady = new Promise((resolve) => {
-    document.onreadystatechange = () => {
-        if (document.readyState === 'complete')
-        {
-            const iframes = document.querySelectorAll('iframe')
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      const iframes = document.querySelectorAll('iframe')
 
-            if (iframes.length) {
-                Promise.all(
-                    [...iframes].map((iframeElem) => new Promise((resolve) => {
-                            iframeElem.contentWindow.addEventListener('message', (e) => {
-                                if (e?.data === 'briskine-ready') {
-                                    resolve(true)
-                                }
-                            })
-                        })
-                    )
-                ).then(() => { 
-                    resolve(true)
-                } )
-            } else {
-                resolve(true)
-            }
-        }
+      if (iframes.length) {
+        Promise.all(
+          [...iframes].map(
+            (iframeElem) =>
+              new Promise((resolve) => {
+                iframeElem.contentWindow.addEventListener('message', (e) => {
+                  if (e?.data === 'briskine-ready') {
+                    resolve()
+                  }
+                })
+              }),
+          ),
+        ).then(() => {
+          resolve()
+        })
+      } else {
+        resolve(true)
+      }
     }
+  }
 })
 
 Promise.all([promiseMainWindow, promiseDocumentReady]).then(() => {
-    // eslint-disable-next-line no-console
-    console.log('BSKN inited')
+  // eslint-disable-next-line no-console
+  console.log('BSKN inited')
 })
