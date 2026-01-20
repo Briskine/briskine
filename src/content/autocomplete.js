29b46ee2d02358948fb7551097023c6e1dfacf1e
Ionut Colceriu <contact@ghinda.net>
2026-01-20 10:28:06 +0200

    Move the universal editor to autocomplete. Since it's not a real editor, but merges the other editors.

--- src/content/autocomplete.js
+++ src/content/autocomplete.js
@@ -5,7 +5,13 @@
 import { run } from './plugin.js'
 import { addAttachments } from './attachments/attachments.js'
 import parseTemplate from './utils/parse-template.js'
-import { insertTemplate } from './editors/editor-universal.js'
+
+import {isContentEditable, insertContentEditableTemplate} from './editors/editor-contenteditable.js'
+import {isCkEditor, insertCkEditorTemplate} from './editors/editor-ckeditor.js'
+import {isPasteEditor, insertPasteTemplate} from './editors/editor-paste.js'
+import {isBeforeInputEditor, insertBeforeInputTemplate} from './editors/editor-beforeinput.js'
+import {isQuill, insertQuillTemplate} from './editors/editor-quill.js'
+import {insertTextareaTemplate} from './editors/editor-textarea.js'
 
 import './plugins/gmail.js'
 import './plugins/outlook.js'
@@ -15,6 +21,30 @@ import './plugins/linkedin-sales-navigator.js'
 import './plugins/facebook.js'
 
 import { updateTemplateStats } from '../store/store-content.js'
+
+function insertTemplate (params = {}) {
+  if (isCkEditor(params.element)) {
+    return insertCkEditorTemplate(params)
+  }
+
+  if (isPasteEditor(params.element)) {
+    return insertPasteTemplate(params)
+  }
+
+  if (isBeforeInputEditor(params.element)) {
+    return insertBeforeInputTemplate(params)
+  }
+
+  if (isQuill(params.element)) {
+    return insertQuillTemplate(params)
+  }
+
+  if (isContentEditable(params.element)) {
+    return insertContentEditableTemplate(params)
+  }
+
+  return insertTextareaTemplate(params)
+}
 
 export default async function autocomplete ({element, word, template}) {
   const withAttachments = addAttachments(template.body, template.attachments)
