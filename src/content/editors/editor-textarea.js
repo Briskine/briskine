d186b50cedbadebc9cf4e818ee7d65ddf356116f
Ionut Colceriu <contact@ghinda.net>
2026-01-20 11:18:41 +0200

    Tweak textarea editor for new params.

--- src/content/editors/editor-textarea.js
+++ src/content/editors/editor-textarea.js
@@ -1,36 +1,32 @@
-/* Generic editors
+/* Textarea and Input
  */
 
-import htmlToText from '../utils/html-to-text.js'
-
-export function insertTextareaTemplate (params = {}) {
+export function insertTextareaTemplate ({ element, word, template, text }) {
   // restore focus to the editable area
-  params.element.focus()
+  element.focus()
 
-  // convert html to text for textfields
-  const content = htmlToText(params.text)
-  const textfieldValue = params.element.value
-  let cursorOffset = params.word.end + content.length
+  const textfieldValue = element.value
+  let cursorOffset = word.end + text.length
 
   // if the current word matches the shortcut then remove it
   // otherwise skip it (ex: from dialog)
-  let wordStart = params.word.start
-  if (params.word.text === params.template.shortcut) {
+  let wordStart = word.start
+  if (word.text === template.shortcut) {
     // decrease the cursor offset with the removed text length
-    cursorOffset = cursorOffset - params.word.text.length
+    cursorOffset = cursorOffset - word.text.length
   } else {
     // don't delete anything in the textarea
     // just add the qt
-    wordStart = params.word.end
+    wordStart = word.end
   }
 
-  params.element.value = textfieldValue.substr(0, wordStart) + content + textfieldValue.substr(params.word.end)
+  element.value = textfieldValue.substr(0, wordStart) + text + textfieldValue.substr(word.end)
 
   // set focus at the end of the added template.
   // only supported on inputs of types text, search, URL, tel and password.
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/setSelectionRange
   try {
-    params.element.setSelectionRange(cursorOffset, cursorOffset)
+    element.setSelectionRange(cursorOffset, cursorOffset)
   } catch {
     // probably unsupported input type
   }
@@ -38,8 +34,6 @@ export function insertTextareaTemplate (params = {}) {
   // trigger multiple change events,
   // for frameworks and scripts to notice changes to the editable fields.
   Array('input', 'change').forEach((eventType) => {
-      params.element.dispatchEvent(new Event(eventType, {bubbles: true}))
+    element.dispatchEvent(new Event(eventType, {bubbles: true}))
   })
-
-  return
 }
