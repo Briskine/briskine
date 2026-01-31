8abb10b8da0388ae904675989aa29df96b5355e1
Ionut Colceriu <contact@ghinda.net>
2026-01-31 15:17:11 +0200

    Add unit tests for the new execcommand fallback editor.

---
+++ src/content/editors/editor-execcommand.spec.js
@@ -0,0 +1,59 @@
+/* globals describe, it, before, after */
+import {expect} from 'chai'
+
+import { insertExecCommandTemplate } from './editor-execcommand.js'
+
+describe('editor ExecCommand', () => {
+  let editable
+  before(() => {
+    editable = document.createElement('div')
+    editable.setAttribute('contenteditable', 'true')
+    document.body.appendChild(editable)
+  })
+
+  it('should insert template into contenteditable', async () => {
+    editable.innerHTML = ''
+    editable.focus()
+
+    await insertExecCommandTemplate({
+      html: '<div>test</div>',
+      element: editable,
+      focusNode: editable,
+      word: {
+        start: 0,
+        end: 0,
+        text: ''
+      },
+      template: {
+        shortcut: ''
+      }
+    })
+
+    expect(editable.innerHTML).to.equal('<div>test</div>')
+  })
+
+  it('should insert template into contenteditable, with preceding text', async () => {
+    editable.innerHTML = '<div>pre</div>'
+    window.getSelection().setBaseAndExtent(editable.firstChild.firstChild, 3, editable.firstChild.firstChild, 3)
+
+    await insertExecCommandTemplate({
+      html: '<div>template <img src="#" alt="image"></div>',
+      element: editable,
+      focusNode: window.getSelection().focusNode,
+      word: {
+        start: 0,
+        end: 0,
+        text: ''
+      },
+      template: {
+        shortcut: ''
+      }
+    })
+
+    expect(editable.innerHTML).to.equal('<div>pretemplate <img src="#" alt="image"></div>')
+  })
+
+  after(() => {
+    editable.remove()
+  })
+})
