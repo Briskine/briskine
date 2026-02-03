ac02b6e222b87375b756a2db8166fd7787c69a5f
Ionut Colceriu <contact@ghinda.net>
2026-02-03 16:31:10 +0200

    Disable browser mode screenshots.

--- vitest.config.ts
+++ vitest.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
           provider: playwright(),
           enabled: true,
           headless: true,
+          screenshotFailures: false,
           instances: [
             { browser: 'chromium' }
           ],
