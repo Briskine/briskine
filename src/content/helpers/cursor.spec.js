736c020f231fae38d3bff82679f9df78cd603adb
Ionut Colceriu <contact@ghinda.net>
2026-02-23 13:39:56 +0200

    Tweak cusor helper unit tests.

--- src/content/helpers/cursor.spec.js
+++ src/content/helpers/cursor.spec.js
@@ -1,54 +1,62 @@
 import { expect, describe, it } from 'vitest'
 
-import {compileTemplate} from '../sandbox/sandbox.js'
+import { compileTemplate } from '../sandbox/sandbox.js'
+import { cursorMarker } from '../cursors/cursors.js'
 
-export const cursorMarker = '\u200B'
+function cursor (placeholder = '') {
+  return cursorMarker + placeholder + cursorMarker
+}
 
 describe('cursor handlebars helper', () => {
   it('should render empty markers', async () => {
-    expect(await compileTemplate('{{cursor}}')).to.equal(cursorMarker + cursorMarker)
+    expect(await compileTemplate('{{cursor}}')).to.equal(cursor())
   })
 
   it('should render placeholder markers when used inline', async () => {
-    expect(await compileTemplate('{{cursor "briskine"}}'))
-      .to.equal(cursorMarker + 'briskine' + cursorMarker)
+    expect(await compileTemplate('{{cursor "placeholder"}}'))
+      .to.equal(cursor('placeholder'))
   })
 
   it('should escape special characters in placeholders when used inline', async () => {
-    expect(await compileTemplate('{{cursor "& > briskine"}}'))
-      .to.equal(cursorMarker + '&amp; &gt; briskine' + cursorMarker)
+    expect(await compileTemplate('{{cursor "& > placeholder"}}'))
+      .to.equal(cursor('&amp; &gt; placeholder'))
   })
 
   it('should render placeholder markers', async () => {
-    expect(await compileTemplate('{{#cursor}}briskine{{/cursor}}'))
-      .to.equal(cursorMarker + 'briskine' + cursorMarker)
+    expect(await compileTemplate('{{#cursor}}placeholder{{/cursor}}'))
+      .to.equal(cursor('placeholder'))
   })
 
   it('should support special characters in placeholder', async () => {
-    expect(await compileTemplate('{{#cursor}}& > briskine{{/cursor}}'))
-      .to.equal(cursorMarker + '& > briskine' + cursorMarker)
+    expect(await compileTemplate('{{#cursor}}& > placeholder{{/cursor}}'))
+      .to.equal(cursor('& > placeholder'))
+  })
+
+  it('should ignore the inline placeholder argument when used as a block', async () => {
+    expect(await compileTemplate('{{#cursor "inline-placeholder"}}block-placeholder{{/cursor}}'))
+      .to.equal(cursor('block-placeholder'))
   })
 
   it('should allow html', async () => {
     expect(await compileTemplate('{{#cursor}}<img src="#">block{{/cursor}}'))
-      .to.equal(cursorMarker + '<img src="#">block' + cursorMarker)
+      .to.equal(cursor('<img src="#">block'))
   })
 
   it('should support dynamic variables', async () => {
-    expect(await compileTemplate('{{#cursor}}{{first_name}}{{/cursor}}', {first_name: 'Briskine'}))
-      .to.equal(cursorMarker + 'Briskine' + cursorMarker)
+    expect(await compileTemplate('{{#cursor}}{{first_name}}{{/cursor}}', {first_name: 'placeholder'}))
+      .to.equal(cursor('placeholder'))
   })
 
   it('should support loop with private handlebars expressions inside cursor', async () => {
-    expect(await compileTemplate('{{#cursor}}{{#each list}}{{@index}} {{this}}{{/each}}{{/cursor}}', {list: ['Briskine']}))
-      .to.equal(cursorMarker + '0 Briskine' + cursorMarker)
+    expect(await compileTemplate('{{#cursor}}{{#each list}}{{@index}} {{this}}{{/each}}{{/cursor}}', {list: ['placeholder']}))
+      .to.equal(cursor('0 placeholder'))
   })
 
   it('should support cursor with expressions inside loop', async () => {
     expect(await compileTemplate('{{#each list}}{{#cursor}}{{@index}} {{this}} {{@root.first_name}}{{/cursor}}{{/each}}', {
-      list: ['Briskine'],
+      list: ['placeholder'],
       first_name: 'First'
     }))
-      .to.equal(cursorMarker + '0 Briskine First' + cursorMarker)
+      .to.equal(cursor('0 placeholder First'))
   })
 })
