e5b46b468b1f413de9692fbc0adfe04fb01d49fd
Ionut Colceriu <contact@ghinda.net>
2026-06-24 10:41:07 +0300

    Make sure we don't insert templates in non-editable elements from the sidebar.

--- src/content/insert-template-event.js
+++ src/content/insert-template-event.js
@@ -1,8 +1,18 @@
 import { eventInsertTemplate } from '../config.js'
 import autocomplete from './autocomplete.js'
 import { on, off } from '../store/store-content.js'
+import { getActiveElement } from './utils/active-element.js'
+import { isTextfieldEditor } from './editors/editor-textfield.js'
+import { isContentEditable } from './editors/editor-contenteditable.js'
 
 function insertTemplate ({ template = {} }) {
+  let element = getActiveElement()
+  // if it's not an editable element
+  // don't trigger anything
+  if (!isTextfieldEditor(element) && !isContentEditable(element)) {
+    return
+  }
+
   autocomplete({
     template: template,
   })
