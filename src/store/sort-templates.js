a51bd49870558ea5e62a5fd95e74e0bd818545db
Ionut Colceriu <contact@ghinda.net>
2026-08-18 16:49:30 +0300

    Replace toSorted with spread to try and keep windows7 support.

--- src/store/sort-templates.js
+++ src/store/sort-templates.js
@@ -2,22 +2,22 @@ const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base
 
 export default function sortTemplates (list = [], sort = 'last_used', lastUsed = {}) {
   if (['title', 'shortcut'].includes(sort)) {
-    return list
-      .toSorted((a, b) => {
+    return [...list]
+      .sort((a, b) => {
         return collator.compare(a[sort], b[sort])
       })
   }
 
   if (sort === 'modified_datetime') {
-    return list
-      .toSorted((a, b) => {
+    return [...list]
+      .sort((a, b) => {
         return new Date(b.modified_datetime || 0) - new Date(a.modified_datetime || 0)
       })
   }
 
   // default last_used sort
-  return list
-    .toSorted((a, b) => {
+  return [...list]
+    .sort((a, b) => {
       return new Date(lastUsed[b.id] || 0) - new Date(lastUsed[a.id] || 0)
     })
 }
